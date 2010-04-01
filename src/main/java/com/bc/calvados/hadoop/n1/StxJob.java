5eec8693eae0a14b78cd9cd7620de03e56c67646
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-04-01 15:09:01 +0200

    convert Job into Tool implementation

--- src/main/java/com/bc/calvados/hadoop/n1/StxJob.java
+++ src/main/java/com/bc/calvados/hadoop/n1/StxJob.java
@@ -1,19 +1,23 @@
 package com.bc.calvados.hadoop.n1;
 
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-import java.io.IOException;
 
 
-public class StxJob {
+public class StxJob extends Configured implements Tool {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        final Job job = new Job();
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = new Job(getConf(), "Stx computation");
+        job.setJarByClass(StxJob.class);
 
         job.setInputFormatClass(N1ProductFormat.class);
 
@@ -32,6 +36,10 @@ public class StxJob {
         FileInputFormat.setInputPaths(job, new Path("input"));
         FileOutputFormat.setOutputPath(job, new Path("output"));
 
-        job.waitForCompletion(true);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.exit(ToolRunner.run(new StxJob(), args));
     }
 }
