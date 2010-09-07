6bcd97c80ea9eda92205a12de060fc3e43a22688
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-09-07 17:01:40 +0200

    extned test coverage of DefaultReferenceDatabase

--- calvados-core/src/test/java/org/esa/beam/gpf/operators/matchup/DefaultReferenceDatabaseTest.java
+++ calvados-core/src/test/java/org/esa/beam/gpf/operators/matchup/DefaultReferenceDatabaseTest.java
@@ -12,28 +12,47 @@ import java.awt.geom.AffineTransform;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class DefaultReferenceDatabaseTest {
 
     @Test
     public void testFindReferenceMeasurement() throws Exception {
         DefaultReferenceDatabase referenceDatabase = new DefaultReferenceDatabase();
-        GeoPos geoPos = new GeoPos(42f, 56f);
-        Date date = ProductData.UTC.parse("22-JAN-2005 11:11:11").getAsDate();
-        String site = "northsea";
-        ReferenceMeasurement rm = new ReferenceMeasurement("id1", site, date, geoPos);
-        referenceDatabase.addReferenceMeasurement(rm);
+
+        Date date1 = ProductData.UTC.parse("22-JAN-2005 11:11:11").getAsDate();
+        String northSeaSite = "northsea";
+        ReferenceMeasurement northseaMeasurement = new ReferenceMeasurement("id1", northSeaSite, date1, new GeoPos(55f, 2f));
+        referenceDatabase.addReferenceMeasurement(northseaMeasurement);
+
+        Date date2 = ProductData.UTC.parse("22-JAN-2005 11:11:11").getAsDate();
+        String gulfSite = "Gulf of Mexico";
+        ReferenceMeasurement gulfMeasurement = new ReferenceMeasurement("id1", gulfSite, date2, new GeoPos(24f, -90f));
+        referenceDatabase.addReferenceMeasurement(gulfMeasurement);
 
         final int deltaTime = 60 * 60;
-        List<ReferenceMeasurement> result = referenceDatabase.findReferenceMeasurement(null, createTestProduct(), deltaTime);
+        Product product = createTestProduct();
+        List<ReferenceMeasurement> result = referenceDatabase.findReferenceMeasurement(null, product, deltaTime);
         assertNotNull(result);
         assertEquals(1, result.size());
+        assertSame(gulfMeasurement, result.get(0));
+
+        result = referenceDatabase.findReferenceMeasurement("africa", product, deltaTime);
+        assertNotNull(result);
+        assertEquals(0, result.size());
+
+        result = referenceDatabase.findReferenceMeasurement(gulfSite, product, deltaTime);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertSame(gulfMeasurement, result.get(0));
+
+        result = referenceDatabase.findReferenceMeasurement(gulfSite, product, 60);
+        assertNotNull(result);
+        assertEquals(0, result.size());
     }
 
     private static Product createTestProduct() throws Exception {
-        Rectangle bounds = new Rectangle(360, 180);
+        Rectangle bounds = new Rectangle(180, 180);
         Product product = new Product("test", "TEST", bounds.width, bounds.height);
 
         product.setStartTime(ProductData.UTC.parse("22-JAN-2005 10:00:00"));
@@ -43,7 +62,7 @@ public class DefaultReferenceDatabaseTest {
         final int northing = 90;
         final int easting = -180;
         i2mTransform.translate(easting, northing);
-        final double scaleX = 360 / bounds.width;
+        final double scaleX = 180 / bounds.width;
         final double scaleY = 180 / bounds.height;
         i2mTransform.scale(scaleX, -scaleY);
         CrsGeoCoding geoCoding = new CrsGeoCoding(DefaultGeographicCRS.WGS84, bounds, i2mTransform);
