adb66b4d6776a378d2d5ddbfd3e8837fefe7f583
Norman Fomferra <norman@ubuntu.(none)>
2010-10-05 17:46:17 +0200

    Added a new assertion.

--- calvalus-experiments/src/test/java/com/bc/calvalus/experiments/add/AddRastersToolTest.java
+++ calvalus-experiments/src/test/java/com/bc/calvalus/experiments/add/AddRastersToolTest.java
@@ -5,33 +5,42 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.ToolRunner;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AddRastersToolTest {
+    private static final String TMP_INPUT = "/tmp/input";
+    private static final String TMP_OUTPUT = "/tmp/output";
+
     @Test
     public void runAddRasters() throws Exception {
-        String[] args = new String[]{
-                "/tmp/input", "/tmp/output"
-        };
-        ToolRunner.run(new AddRastersTool(), args);
-
         LocalFileSystem fileSystem = LocalFileSystem.getLocal(new Configuration());
-        final Path file = new Path(args[1], "part-r-00000");
-        assertTrue(fileSystem.exists(file));
-        FSDataInputStream isOutput = fileSystem.open(file);
+        Path outputFile = new Path(TMP_OUTPUT, "part-r-00000");
+
+        assertTrue("Shall not exist: " + outputFile,
+                   !fileSystem.exists(outputFile));
+
+        // Run the map-reduce job
+        ToolRunner.run(new AddRastersTool(), new String[]{
+                TMP_INPUT, TMP_OUTPUT
+        });
+
+        assertTrue("Shall now exist: " + outputFile,
+                   fileSystem.exists(outputFile));
+
+        FSDataInputStream isOutput = fileSystem.open(outputFile);
         for (int i = 0; i < 4; i++) {
-            assertEquals(AddRastersTool.generateInputRasterPixel(0, i)
-                    + AddRastersTool.generateInputRasterPixel(1, i)
-                    + AddRastersTool.generateInputRasterPixel(2, i),
-                                isOutput.read());
+            assertEquals("Invalid output pixel " + i,
+                         AddRastersTool.generateInputRasterPixel(0, i)
+                                 + AddRastersTool.generateInputRasterPixel(1, i)
+                                 + AddRastersTool.generateInputRasterPixel(2, i),
+                         isOutput.read());
         }
 
-        fileSystem.delete(new Path(args[0]), true);
-        fileSystem.delete(new Path(args[1]), true);
+        fileSystem.delete(new Path(TMP_INPUT), true);
+        fileSystem.delete(new Path(TMP_OUTPUT), true);
     }
 
 }
