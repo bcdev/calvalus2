6721e901d2e0a17af27dd4eea11ea72dc3c34c23
martin-boettcher <martin.boettcher@brockmann-consult.de>
2010-10-25 11:56:07 +0200

    in preparation of second test run

--- calvalus-experiments/bin/run2010-10-20.sh
+++ calvalus-experiments/bin/run2010-10-20.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
+BIN_DIR=`dirname $0`
 (
 for i in 1 2 3
 do
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n1 ndvi
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n3 ndvi
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n3 ndvi -splits=18
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/sliced/MERIS-RR-product sliced ndvi
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/lineinterleaved/MERIS-RR-product lineinterleaved ndvi
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n1 ndvi
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n3 ndvi
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n3 ndvi -splits=18
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/sliced/MERIS-RR-product sliced ndvi
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/lineinterleaved/MERIS-RR-product lineinterleaved ndvi
 
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n1 ndvi
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n3 ndvi
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n3 ndvi -splits=18
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/sliced/MERIS-RR-2010-08 sliced ndvi
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/lineinterleaved/MERIS-RR-2010-08 lineinterleaved ndvi
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n1 ndvi
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n3 ndvi
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n3 ndvi -splits=18
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/sliced/MERIS-RR-2010-08 sliced ndvi
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/lineinterleaved/MERIS-RR-2010-08 lineinterleaved ndvi
 
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n1 radiometry
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n3 radiometry
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n3 radiometry -splits=18
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/sliced/MERIS-RR-product sliced radiometry
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/lineinterleaved/MERIS-RR-product lineinterleaved radiometry
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n1 radiometry
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n3 radiometry
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-product n3 radiometry -splits=18
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/sliced/MERIS-RR-product sliced radiometry
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/lineinterleaved/MERIS-RR-product lineinterleaved radiometry
 
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n1 radiometry
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n3 radiometry
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n3 radiometry -splits=18
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/sliced/MERIS-RR-2010-08 sliced radiometry
-../bin/runprocessing2 hdfs://cvmaster00:9000/data/experiments/lineinterleaved/MERIS-RR-2010-08 lineinterleaved radiometry
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n1 radiometry
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n3 radiometry
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/n1/MERIS-RR-2010-08 n3 radiometry -splits=18
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/sliced/MERIS-RR-2010-08 sliced radiometry
+    $BIN_DIR/runprocessing2 hdfs://cvmaster00:9000/data/experiments/lineinterleaved/MERIS-RR-2010-08 lineinterleaved radiometry
 done
 ) 2>&1 | tee output.list 
 
