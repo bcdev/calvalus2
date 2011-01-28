1c482ea4e447f2a19d6fba1c43cca74873fb5495
unknown <Norman@.(none)>
2011-01-28 15:33:19 +0100

    Added new processing CLI client for BEAM GPF operators

---
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/beam/BeamOperatorToolTest.java
@@ -0,0 +1,51 @@
+package com.bc.calvalus.processing.beam;
+
+import com.bc.calvalus.processing.shellexec.ExecutablesTool;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.ToolRunner;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for {@link BeamOperatorTool}.
+ */
+public class BeamOperatorToolTest {
+    private static final String REQUEST = "calvalus-processing/src/test/resources/l2gen-local-request.xml";
+    private static final String OUTPUT_DIR = "file:///tmp/meris-l2gen-99";
+
+    @Test
+    public void testCommandLine() throws Exception {
+        
+        // verify that output is empty
+        Path outputFile = new Path(OUTPUT_DIR);
+        assertTrue("Shall not exist: " + outputFile,
+                   ! outputFile.getFileSystem(new Configuration()).exists(outputFile));
+
+        // Run the job
+        ToolRunner.run(new ExecutablesTool(), new String[] { REQUEST });
+
+        assertTrue("Shall now exist: " + outputFile,
+                   outputFile.getFileSystem(new Configuration()).exists(outputFile));
+    }
+
+    @Before
+    public void init() throws IOException {
+        clearWorkingDirs();
+    }
+
+//    @After
+//    public void finish() throws IOException {
+//        clearWorkingDirs();
+//    }
+
+    private void clearWorkingDirs() throws IOException {
+        Path outputDir = new Path(OUTPUT_DIR);
+        outputDir.getFileSystem(new Configuration()).delete(outputDir, true);
+    }
+
+}
