64b5b929453b87d5c40bc9836ae4d9f2f9449d7f
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-02-23 17:38:23 +0100

    Updated UI

--- calvalus-portal/src/main/java/com/bc/calvalus/portal/client/L3ProcessorPanel.java
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/client/L3ProcessorPanel.java
@@ -3,12 +3,12 @@ package com.bc.calvalus.portal.client;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.DoubleBox;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.IntegerBox;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
@@ -34,15 +34,14 @@ public class L3ProcessorPanel implements IsWidget {
     private TextBox validMask;
     private ListBox aggregator;
     private DoubleBox weightCoeff;
-    private DateBox fromDate;
-    private DateBox toDate;
+    private DateBox dateStart;
+    private DateBox dateStop;
     private IntegerBox periodLength;
-    private DoubleBox fromLon;
-    private DoubleBox toLon;
-    private DoubleBox fromLat;
-    private DoubleBox toLat;
+    private DoubleBox lonMin;
+    private DoubleBox lonMax;
+    private DoubleBox latMin;
+    private DoubleBox latMax;
     private DoubleBox resolution;
-    private CheckBox multiPeriodOn;
     private IntegerBox periodCount;
     private IntegerBox superSampling;
 
@@ -72,6 +71,9 @@ public class L3ProcessorPanel implements IsWidget {
         contentParams.setCellSpacing(2);
         contentParams.getFlexCellFormatter().setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
         contentParams.getFlexCellFormatter().setColSpan(0, 0, 3);
+        contentParams.getFlexCellFormatter().setColSpan(1, 1, 2);
+        contentParams.getFlexCellFormatter().setColSpan(2, 1, 2);
+        contentParams.getFlexCellFormatter().setColSpan(3, 1, 2);
         contentParams.setWidget(0, 0, new HTML("<b>L3 Content Parameters</b>"));
         contentParams.setWidget(1, 0, new Label("Input variable(s):"));
         contentParams.setWidget(1, 1, variables);
@@ -81,29 +83,22 @@ public class L3ProcessorPanel implements IsWidget {
         contentParams.setWidget(3, 1, this.aggregator);
         contentParams.setWidget(4, 0, new Label("Weight coeff.:"));
         contentParams.setWidget(4, 1, this.weightCoeff);
+        contentParams.setWidget(4, 2, new Label("1"));
         contentParams.setWidget(5, 0, new Label("Super-sampling:"));
         contentParams.setWidget(5, 1, this.superSampling);
+        contentParams.setWidget(5, 2, new Label("pixel/pixel"));
 
-        fromDate = new DateBox();
-        fromDate.setFormat(new DateBox.DefaultFormat(DATE_FORMAT));
-        fromDate.setValue(DATE_FORMAT.parse("2008-06-01"));
+        dateStart = new DateBox();
+        dateStart.setFormat(new DateBox.DefaultFormat(DATE_FORMAT));
+        dateStart.setValue(DATE_FORMAT.parse("2008-06-01"));
 
-        toDate = new DateBox();
-        toDate.setFormat(new DateBox.DefaultFormat(DATE_FORMAT));
-        toDate.setValue(DATE_FORMAT.parse("2008-06-07"));
-        toDate.addValueChangeHandler(new ValueChangeHandler<Date>() {
+        dateStop = new DateBox();
+        dateStop.setFormat(new DateBox.DefaultFormat(DATE_FORMAT));
+        dateStop.setValue(DATE_FORMAT.parse("2008-06-07"));
+        dateStop.addValueChangeHandler(new ValueChangeHandler<Date>() {
             @Override
             public void onValueChange(ValueChangeEvent<Date> event) {
                 updateTimeParams(true);
-            }
-        });
-
-        multiPeriodOn = new CheckBox("Multi-period (time-series)");
-        multiPeriodOn.setValue(false);
-        multiPeriodOn.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<Boolean> event) {
-                updateTimeParams(false);
             }
         });
 
@@ -125,34 +120,48 @@ public class L3ProcessorPanel implements IsWidget {
             }
         });
 
+        HorizontalPanel timeRange = new HorizontalPanel();
+        timeRange.add(dateStart);
+        timeRange.add(new Label("-"));
+        timeRange.add(dateStop);
+
+        HorizontalPanel period = new HorizontalPanel();
+        period.add(periodCount);
+        period.add(new Label("x"));
+        period.add(periodLength);
+
         FlexTable temporalParams = new FlexTable();
         temporalParams.setWidth("100%");
         temporalParams.setCellSpacing(2);
         temporalParams.getFlexCellFormatter().setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
         temporalParams.getFlexCellFormatter().setColSpan(0, 0, 3);
-        temporalParams.getFlexCellFormatter().setColSpan(3, 0, 3);
+        temporalParams.getFlexCellFormatter().setColSpan(1, 1, 2);
         temporalParams.setWidget(0, 0, new HTML("<b>L3 Temporal Parameters</b>"));
-        temporalParams.setWidget(1, 0, new Label("From date:"));
-        temporalParams.setWidget(1, 1, fromDate);
-        temporalParams.setWidget(2, 0, new Label("To date:"));
-        temporalParams.setWidget(2, 1, toDate);
-        temporalParams.setWidget(3, 0, multiPeriodOn);
-        temporalParams.setWidget(4, 0, new Label("Period count:"));
-        temporalParams.setWidget(4, 1, periodCount);
-        temporalParams.setWidget(4, 2, new Label("periods"));
-        temporalParams.setWidget(5, 0, new Label("Period length:"));
-        temporalParams.setWidget(5, 1, periodLength);
-        temporalParams.setWidget(5, 2, new Label("days"));
+        temporalParams.setWidget(1, 0, new Label("Time range:"));
+        temporalParams.setWidget(1, 1, timeRange);
+        temporalParams.setWidget(2, 0, new Label("Period:"));
+        temporalParams.setWidget(2, 1, period);
+        temporalParams.setWidget(2, 2, new Label("days"));
 
-        fromLon = new DoubleBox();  // todo - validate against -180 <= x <= 180
-        fromLon.setValue(-180.0);
-        toLon = new DoubleBox();   // todo - validate against -180 <= x <= 180
-        toLon.setValue(180.0);
+        lonMin = new DoubleBox();  // todo - validate against -180 <= x <= 180
+        lonMin.setValue(-180.0);
+        lonMax = new DoubleBox();   // todo - validate against -180 <= x <= 180
+        lonMax.setValue(180.0);
 
-        fromLat = new DoubleBox();  // todo - validate against -90 <= x <= 90
-        fromLat.setValue(90.0);
-        toLat = new DoubleBox();  // todo - validate against -90 <= x <= 90
-        toLat.setValue(90.0);
+        HorizontalPanel lonRange = new HorizontalPanel();
+        lonRange.add(lonMin);
+        lonRange.add(new Label("-"));
+        lonRange.add(lonMax);
+
+        latMin = new DoubleBox();  // todo - validate against -90 <= x <= 90
+        latMin.setValue(-90.0);
+        latMax = new DoubleBox();  // todo - validate against -90 <= x <= 90
+        latMax.setValue(90.0);
+
+        HorizontalPanel latRange = new HorizontalPanel();
+        latRange.add(latMin);
+        latRange.add(new Label("-"));
+        latRange.add(latMax);
 
         resolution = new DoubleBox();   // todo - validate against 0 < x <= 10
         resolution.setValue(0.0416667);
@@ -163,21 +172,15 @@ public class L3ProcessorPanel implements IsWidget {
         spatialParams.getFlexCellFormatter().setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
         spatialParams.getFlexCellFormatter().setColSpan(0, 0, 3);
         spatialParams.setWidget(0, 0, new HTML("<b>L3 Spatial Parameters</b>"));
-        spatialParams.setWidget(1, 0, new Label("From longitude:"));
-        spatialParams.setWidget(1, 1, fromLon);
-        spatialParams.setWidget(1, 2, new Label("degree"));
-        spatialParams.setWidget(2, 0, new Label("To longitude:"));
-        spatialParams.setWidget(2, 1, toLon);
-        spatialParams.setWidget(2, 2, new Label("degree"));
-        spatialParams.setWidget(3, 0, new Label("From latitude:"));
-        spatialParams.setWidget(3, 1, fromLat);
-        spatialParams.setWidget(3, 2, new Label("degree"));
-        spatialParams.setWidget(4, 0, new Label("To latitude:"));
-        spatialParams.setWidget(4, 1, toLat);
-        spatialParams.setWidget(4, 2, new Label("degree"));
-        spatialParams.setWidget(5, 0, new Label("Resolution:"));
-        spatialParams.setWidget(5, 1, resolution);
-        spatialParams.setWidget(5, 2, new Label("degree/pixel"));
+        spatialParams.setWidget(1, 0, new Label("Long. range:"));
+        spatialParams.setWidget(1, 1, lonRange);
+        spatialParams.setWidget(1, 2, new Label("deg"));
+        spatialParams.setWidget(2, 0, new Label("Lat. range:"));
+        spatialParams.setWidget(2, 1, latRange);
+        spatialParams.setWidget(2, 2, new Label("deg"));
+        spatialParams.setWidget(3, 0, new Label("Resolution:"));
+        spatialParams.setWidget(3, 1, resolution);
+        spatialParams.setWidget(3, 2, new Label("deg/pixel"));
 
         VerticalPanel panel = new VerticalPanel();
         panel.setSpacing(4);
@@ -194,37 +197,20 @@ public class L3ProcessorPanel implements IsWidget {
     boolean adjustingEndTime;
 
     private void updateTimeParams(boolean endTimeAdjusted) {
-        if (adjustingEndTime) {
-            return;
-        }
-        long millisPerDay = 24L * 60L * 60L * 1000L;
-        long deltaMillis = toDate.getValue().getTime() - fromDate.getValue().getTime();
-        int deltaDays = (int) ((millisPerDay + deltaMillis - 1) / millisPerDay);
-        ;
+        if (!adjustingEndTime) {
+            long millisPerDay = 24L * 60L * 60L * 1000L;
+            long deltaMillis = dateStop.getValue().getTime() - dateStart.getValue().getTime();
+            int deltaDays = (int) ((millisPerDay + deltaMillis - 1) / millisPerDay);
 
-        if (endTimeAdjusted) {
-            if (multiPeriodOn.getValue()) {
-                // fix 'fromDate', 'toDate' and 'periodLength'
+            if (endTimeAdjusted) {
                 periodCount.setValue(1 + (deltaDays - 1) / periodLength.getValue());
             } else {
-                // fix 'fromDate', 'toDate'. 'periodCount' is one.
-                periodCount.setValue(1);
-                periodLength.setValue(deltaDays);
-            }
-        } else {
-            try {
-                adjustingEndTime = true;
-                if (multiPeriodOn.getValue()) {
-                    // fix 'fromDate' and 'periodLength'
-                    periodCount.setValue(deltaDays / periodLength.getValue());
-                    toDate.setValue(new Date(fromDate.getValue().getTime() + periodCount.getValue() * periodLength.getValue() * millisPerDay));
-                } else {
-                    // fix 'fromDate' and 'periodLength'. 'periodCount' is one.
-                    periodCount.setValue(1);
-                    toDate.setValue(new Date(fromDate.getValue().getTime() + periodLength.getValue() * millisPerDay));
+                try {
+                    adjustingEndTime = true;
+                    dateStop.setValue(new Date(dateStart.getValue().getTime() + periodCount.getValue() * periodLength.getValue() * millisPerDay));
+                } finally {
+                    adjustingEndTime = false;
                 }
-            } finally {
-                adjustingEndTime = false;
             }
         }
     }
@@ -241,16 +227,16 @@ public class L3ProcessorPanel implements IsWidget {
         sb.append(createParameterElement("validMask", validMask.getText()));
         sb.append(createParameterElement("aggregator", aggregator.getValue(aggregator.getSelectedIndex())));
         sb.append(createParameterElement("weightCoeff", weightCoeff.getText()));
-        sb.append(createParameterElement("superSampling", superSampling.getText()));
-        sb.append(createParameterElement("fromDate", fromDate.getFormat().format(fromDate, fromDate.getValue())));
-        sb.append(createParameterElement("toDate", toDate.getFormat().format(toDate, toDate.getValue())));
-        sb.append(createParameterElement("periodLength", periodLength.getText()));
+        sb.append(createParameterElement("dateStart", dateStart.getFormat().format(dateStart, dateStart.getValue())));
+        sb.append(createParameterElement("dateStop", dateStop.getFormat().format(dateStop, dateStop.getValue())));
         sb.append(createParameterElement("periodCount", periodCount.getText()));
-        sb.append(createParameterElement("fromLon", fromLon.getText()));
-        sb.append(createParameterElement("toLon", toLon.getText()));
-        sb.append(createParameterElement("fromLat", fromLat.getText()));
-        sb.append(createParameterElement("toLat", toLat.getText()));
+        sb.append(createParameterElement("periodLength", periodLength.getText()));
+        sb.append(createParameterElement("lonMin", lonMin.getText()));
+        sb.append(createParameterElement("lonMax", lonMax.getText()));
+        sb.append(createParameterElement("latMin", latMin.getText()));
+        sb.append(createParameterElement("latMax", latMax.getText()));
         sb.append(createParameterElement("resolution", resolution.getText()));
+        sb.append(createParameterElement("superSampling", superSampling.getText()));
         sb.append("</parameters>");
         return sb.toString();
     }
