82faf6fb21e4d07556371b67ffe3290b6e3adc03
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2011-02-25 09:12:04 +0100

    improve output converter to work with request files and whole directories

---
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/Config.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package com.bc.calvalus.processing.beam;
+
+/**
+ * Configuration constants.
+ */
+public class Config {
+    public static final String CALVALUS_IDENTIFIER = "calvalus.identifier";
+    public static final String CALVALUS_BUNDLE = "calvalus.bundle";
+    public static final String CALVALUS_INPUT = "calvalus.input";
+    public static final String CALVALUS_OUTPUT = "calvalus.output";
+    public static final String CALVALUS_L2_OPERATOR = "calvalus.l2.operator";
+    public static final String CALVALUS_L2_PARAMETER = "calvalus.l2.parameter";
+    public static final String CALVALUS_L3_PARAMETER = "calvalus.l3.parameter";
+}
