681b5e5a0276483bb6db3c4c8ff8da6b174e4186
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-03-11 18:19:10 +0100

    Added some first code for the L3 Trend Analysis

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/BeamOpProcessingType.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/BeamOpProcessingType.java
@@ -26,6 +26,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.RunningJob;
+import org.apache.hadoop.mapred.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobID;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -33,6 +34,8 @@ import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.esa.beam.util.StringUtils;
 
 import java.util.Map;
+
+import java.util.ArrayList;
 
 /**
  * Creates a beam hadoop job
@@ -121,7 +124,39 @@ public class BeamOpProcessingType {
             job.setOutputValueClass(TemporalBin.class);
 
             job.setOutputFormatClass(SequenceFileOutputFormat.class);
-            // todo - scan all input paths, collect all products and compute min start/ max stop sensing time
+
+            if (configuration.getBoolean("calvalus.l3ta", false)) {
+
+                // todo - this doesn't work! Grrrr!
+                Job job2 = new Job(jobClient.getConf(), "calvalus.l3ta");
+                job2.setNumReduceTasks(4);
+
+                job2.setMapperClass(L3Mapper.class);
+                job2.setMapOutputKeyClass(LongWritable.class);
+                job2.setMapOutputValueClass(SpatialBin.class);
+
+                job2.setPartitionerClass(L3Partitioner.class);
+
+                job2.setReducerClass(L3Reducer.class);
+                job2.setOutputKeyClass(LongWritable.class);
+                job2.setOutputValueClass(TemporalBin.class);
+
+                job2.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+
+                org.apache.hadoop.mapred.jobcontrol.Job jc1 = new org.apache.hadoop.mapred.jobcontrol.Job((JobConf) job.getConfiguration());
+                ArrayList<org.apache.hadoop.mapred.jobcontrol.Job> dependingJobs = new ArrayList<org.apache.hadoop.mapred.jobcontrol.Job>();
+                dependingJobs.add(jc1);
+                JobControl jobControl = new JobControl("calvalus.l3ta");
+                org.apache.hadoop.mapred.jobcontrol.Job jc2 = new org.apache.hadoop.mapred.jobcontrol.Job((JobConf) job2.getConfiguration(),
+                                                                                                          dependingJobs);
+                jobControl.addJob(jc1);
+                jobControl.addJob(jc2);
+                jobControl.run();
+
+            }
+
+
         } else {
             job.setMapperClass(BeamOperatorMapper.class);
             job.setNumReduceTasks(0);
---
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/L3TAMapper.java
@@ -0,0 +1,92 @@
+package com.bc.calvalus.processing.beam;
+
+import com.bc.calvalus.binning.BinningGrid;
+import com.bc.calvalus.binning.TemporalBin;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * Maps the output of L3Reducer to given geographical regions.
+ */
+public class L3TAMapper extends Mapper<LongWritable, TemporalBin, IntWritable, TemporalBin> implements Configurable {
+    private Configuration conf;
+    private BinningGrid binningGrid;
+    private ROI[] rois;
+    private GeometryFactory geometryFactory;
+
+    @Override
+    protected void map(LongWritable key, TemporalBin value, Context context) throws IOException, InterruptedException {
+        double[] centerLonLat = binningGrid.getCenterLonLat(key.get());
+        Point point = geometryFactory.createPoint(new Coordinate(centerLonLat[0], centerLonLat[1]));
+        for (int i = 0; i < rois.length; i++) {
+            ROI roi = rois[i];
+            if (roi.geometry.contains(point)) {
+                 context.write(new IntWritable(i), value);
+            }
+        }
+    }
+
+    @Override
+    public void setConf(Configuration conf) {
+        this.conf = conf;
+        String level3Parameters = new ProcessingConfiguration(conf).getLevel3Parameters();
+        L3Config l3Config = L3Config.create(level3Parameters);
+        binningGrid = l3Config.getBinningContext().getBinningGrid();
+
+        geometryFactory = new GeometryFactory();
+        rois = getRegionsOfInterest(conf);
+    }
+
+    private ROI[] getRegionsOfInterest(Configuration conf) {
+        return new ROI[]{
+                new ROI("region1",
+                        geometryFactory.createPolygon(geometryFactory.createLinearRing(
+                                new Coordinate[]{
+                                        new Coordinate(20, 20),
+                                }
+                        ), null),
+                        0.0),
+                new ROI("region2",
+                        geometryFactory.createPolygon(geometryFactory.createLinearRing(
+                                new Coordinate[]{
+                                        new Coordinate(20, 20),
+                                }
+                        ), null),
+                        0.0),
+                new ROI("region3",
+                        geometryFactory.createPolygon(geometryFactory.createLinearRing(
+                                new Coordinate[]{
+                                        new Coordinate(20, 20),
+                                }
+                        ), null),
+                        0.0),
+        };
+    }
+
+    @Override
+    public Configuration getConf() {
+        return conf;
+    }
+
+
+    public static class ROI {
+        String name;
+        Geometry geometry;
+        double minDepth;
+
+        public ROI(String name, Geometry geometry, double minDepth) {
+            this.name = name;
+            this.geometry = geometry;
+            this.minDepth = minDepth;
+        }
+    }
+}
