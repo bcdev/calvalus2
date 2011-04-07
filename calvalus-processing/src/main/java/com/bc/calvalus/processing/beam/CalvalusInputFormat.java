9cb617704216f5610fd0972e8e6fac66481210f9
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2011-04-07 19:28:08 +0200

    better edge case handling

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/CalvalusInputFormat.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/CalvalusInputFormat.java
@@ -13,6 +13,7 @@ import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import sun.security.krb5.internal.LoginOptions;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -43,33 +44,31 @@ public class CalvalusInputFormat extends InputFormat {
 
             // create splits for each calvalus.input in request
             List<FileSplit> splits = new ArrayList<FileSplit>(requestInputPaths.length);
-            for (int i = 0; i < requestInputPaths.length; ++i) {
+            for (String inputUrl : requestInputPaths) {
                 // get input out of request
-                String inputUrl = requestInputPaths[i];
                 // inquire "status" of file from HDFS
                 Path input = new Path(inputUrl);
                 FileSystem fs = input.getFileSystem(configuration);
                 FileStatus[] file = fs.listStatus(input);
-                if (file.length != 1) throw new FileNotFoundException(inputUrl + " not found");
-                long length = file[0].getLen();
-                BlockLocation[] locations = fs.getFileBlockLocations(file[0], 0, length);
-                // create file split for the input
-                FileSplit split = new FileSplit(input, 0, length, locations[0].getHosts());
-                splits.add(split);
-                LOG.info("split " + inputUrl);
+                if (file.length == 1) {
+                    long length = file[0].getLen();
+                    BlockLocation[] locations = fs.getFileBlockLocations(file[0], 0, length);
+                    if (locations != null && locations.length != 0) {
+                        // create file split for the input
+                        FileSplit split = new FileSplit(input, 0, length, locations[0].getHosts());
+                        splits.add(split);
+                        LOG.info("split " + inputUrl);
+                    } else {
+                        LOG.warning("failed to fs.getFileBlockLocations for: " + inputUrl);
+                    }
+                } else {
+                    LOG.warning(inputUrl + " not found");
+                }
             }
             return splits;
-
         } catch (IOException e) {
-
             LOG.log(Level.SEVERE, "failed to split input set: " + e.toString(), e);
             throw e;
-
-        } catch (Exception e) {
-
-            LOG.log(Level.SEVERE, "failed to split input set: " + e.toString(), e);
-            throw new IOException("failed to split input set: " + e.toString(), e);
-
         }
     }
 
