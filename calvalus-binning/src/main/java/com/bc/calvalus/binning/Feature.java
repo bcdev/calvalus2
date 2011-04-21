0755766efde956bb76c912e600ad512f44718da5
Norman <norman.fomferra@brockmann-consult.de>
2011-04-21 10:24:41 +0200

    New interfaces used to mode statistal features

---
+++ calvalus-binning/src/main/java/com/bc/calvalus/binning/Feature.java
@@ -0,0 +1,13 @@
+package com.bc.calvalus.binning;
+
+/**
+ * A named statistics feature.
+ *
+ * @author Norman
+ */
+public interface Feature {
+    /**
+     * @return The feature's name.
+     */
+    String getName();
+}
---
+++ calvalus-binning/src/main/java/com/bc/calvalus/binning/OutputFeature.java
@@ -0,0 +1,8 @@
+package com.bc.calvalus.binning;
+
+/**
+ * @author Norman
+ */
+public interface OutputFeature extends Feature {
+    float getFillValue();
+}
