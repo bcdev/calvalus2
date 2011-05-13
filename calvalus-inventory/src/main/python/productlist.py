6dc13bea306c5f695d48cb15df004a712acc8602
martin-boettcher <martin.boettcher@brockmann-consult.de>
2011-05-13 15:38:31 +0200

    tools to find overlapping ENVISAT products added to inventory software

---
+++ calvalus-inventory/src/main/python/productlist.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+fileList = []
+rootdir = sys.argv[1]
+for root, subFolders, files in os.walk(rootdir):
+    for file in files:
+        if file.endswith('.N1'):
+            fileList.append(file)
+
+fileList.sort()
+
+for f in fileList:
+    print f
---
+++ calvalus-inventory/src/main/python/productsoverlapping.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import datetime
+
+# usage: python productlist.py /some/root/dir | python productsoverlapping.py >> overlaps ; done
+# MER_FSG_1PNMAP20061111_151656_000000702052_00469_24571_0001.N1
+
+def start(f):
+    return datetime.datetime(int(f[14:18]), int(f[18:20]), int(f[20:22]), int(f[23:25]), int(f[25:27]), int(f[27:29]))
+
+def stop(f):
+    start = datetime.datetime(int(f[14:18]), int(f[18:20]), int(f[20:22]), int(f[23:25]), int(f[25:27]), int(f[27:29]))
+    duration = datetime.timedelta(0,int(f[30:38]))
+    return start + duration
+
+files = sys.stdin.readlines()
+files = map(lambda x:x[:-1], files)
+
+for i in range(len(files)):
+    for j in range(i+1,len(files)):
+        if start(files[j]) >= stop(files[i]):
+            break;
+        # overlapping but not covering: look for smaller one
+        if start(files[j]) > start(files[i]) and stop(files[j]) > stop(files[i]):
+            if stop(files[i]) - start(files[i]) > stop(files[j]) - start(files[j]):
+                print files[j], files[i], stop(files[i]) - start(files[j]), stop(files[j]) - stop(files[i])
+            else:
+                print files[i], files[j], stop(files[i]) - start(files[j]), start(files[j]) - start(files[i])
