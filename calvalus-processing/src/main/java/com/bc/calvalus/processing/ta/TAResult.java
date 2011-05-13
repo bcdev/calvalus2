ba90ae29cc73a059af2baaa12da4907de4e73d02
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2011-05-13 15:23:09 +0200

    fix test

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/ta/TAResult.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/ta/TAResult.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
 package com.bc.calvalus.processing.ta;
 
 import com.bc.calvalus.binning.Vector;
@@ -17,7 +33,7 @@ import java.util.Set;
  */
 public class TAResult {
 
-    private ArrayList<String> header;
+    private List<String> header = new ArrayList<String>();
     private HashMap<String, ArrayList<Record>> tables = new HashMap<String, ArrayList<Record>>();
 
     public void setOutputFeatureNames(String... outputFeatureNames) {
