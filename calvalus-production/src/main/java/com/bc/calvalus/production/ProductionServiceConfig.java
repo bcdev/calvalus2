aedacf701d127960aab702e363e37749aa2676ec
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2011-08-09 16:07:01 +0200

    fix: handle non existing config file

--- calvalus-production/src/main/java/com/bc/calvalus/production/ProductionServiceConfig.java
+++ calvalus-production/src/main/java/com/bc/calvalus/production/ProductionServiceConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
 package com.bc.calvalus.production;
 
 import java.io.File;
@@ -22,15 +38,17 @@ public class ProductionServiceConfig {
 
     public static Map<String, String> loadConfig(File configFile, Map<String, String> defaultConfig) throws IOException {
         Map<String, String> calvalusConfig = defaultConfig != null ? new HashMap<String, String>(defaultConfig) : new HashMap<String, String>();
-        FileReader reader = new FileReader(configFile);
-        Properties properties = new Properties();
-        try {
-            properties.load(reader);
-            overwriteConfigWithProperties(calvalusConfig, properties, null);
-            overwriteConfigWithProperties(calvalusConfig, System.getProperties(), "calvalus.");
-        } finally {
-            reader.close();
+        if (configFile != null && configFile.canRead()) {
+            FileReader reader = new FileReader(configFile);
+            try {
+                Properties properties = new Properties();
+                properties.load(reader);
+                overwriteConfigWithProperties(calvalusConfig, properties, null);
+            } finally {
+                reader.close();
+            }
         }
+        overwriteConfigWithProperties(calvalusConfig, System.getProperties(), "calvalus.");
         return calvalusConfig;
     }
 
