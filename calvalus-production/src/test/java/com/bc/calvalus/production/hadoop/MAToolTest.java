8b66a0678b079039fa691a1f5e0bdc4b1439f92b
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2011-08-09 09:34:58 +0200

    added more tests to WPS --> ProductionRequest conversion

--- calvalus-production/src/test/java/com/bc/calvalus/production/hadoop/MAToolTest.java
+++ calvalus-production/src/test/java/com/bc/calvalus/production/hadoop/MAToolTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
-import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -37,8 +36,20 @@ public class MAToolTest {
         assertNotNull(productionRequest.getUserName());
         Map<String, String> parameters = productionRequest.getParameters();
         assertNotNull(parameters);
-        Set<Map.Entry<String,String>> entries = parameters.entrySet();
-        assertEquals(8, entries.size());
+        assertEquals(8, parameters.size());
+
+        assertTrue(parameters.containsKey("calvalus.processor.package"));
+        assertEquals("beam-meris-radiometry", parameters.get("calvalus.processor.package"));
+
+        assertTrue(parameters.containsKey("calvalus.input"));
+        assertEquals("hdfs://cvmaster00:9000/calvalus/eodata/MER_RR__1P/r03/2004/07/15/MER_RR__1PRACR20040715_011806_000026382028_00332_12410_0000.N1", parameters.get("calvalus.input"));
+
+        assertTrue(parameters.containsKey("calvalus.l2.parameters"));
+        String l2Parameters = parameters.get("calvalus.l2.parameters");
+        assertTrue(l2Parameters.startsWith("<parameters>"));
+        assertTrue(l2Parameters.endsWith("</parameters>"));
+        assertTrue(l2Parameters.contains("<doSmile>true</doSmile>"));
+        assertTrue(l2Parameters.contains("<reproVersion>AUTO_DETECT</reproVersion>"));
     }
 
 }
