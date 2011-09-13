2efec9ac0abf2b981ba9212c8e6308c2ad8f9b98
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-09-13 17:55:16 +0200

    Added a progress box

--- calvalus-portal/src/main/java/com/bc/calvalus/portal/client/OrderProductionView.java
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/client/OrderProductionView.java
@@ -4,6 +4,8 @@ import com.bc.calvalus.portal.shared.DtoProductionRequest;
 import com.bc.calvalus.portal.shared.DtoProductionResponse;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
 
 // todo: prefer composition over inheritance: make subclasses a component of OrderProductionView (which is then concrete) (nf)
 
@@ -57,12 +59,29 @@ public abstract class OrderProductionView extends PortalView {
     protected void orderProduction() {
         if (validateForm()) {
             DtoProductionRequest request = getProductionRequest();
+
+            final DialogBox progressBox = new DialogBox();
+            progressBox.ensureDebugId("cwDialogBox");
+            progressBox.setText("Submitting Production Request...");
+            progressBox.setGlassEnabled(true);
+            progressBox.setAnimationEnabled(true);
+            progressBox.setAutoHideEnabled(true);
+            progressBox.setWidget(new HTML("Your production request is currently being processed on the server. After a preparation phase<br/>" +
+                                                   "you can observe its processing progress in the <b>Manage Productions</b> tab.<br/>" +
+                                                   "Depending on the number of files in the input file set, the preparation phase may take<br/>" +
+                                                   "seconds to minutes.<br/>" +
+                                                   "<br/>" +
+                                                   "This dialog box will then close automatically."));
+            progressBox.show();
+
             getPortal().getBackendService().orderProduction(request, new AsyncCallback<DtoProductionResponse>() {
                 public void onSuccess(final DtoProductionResponse response) {
+                    progressBox.hide();
                     onOrderProductionSuccess();
                 }
 
                 public void onFailure(Throwable caught) {
+                    progressBox.hide();
                     onOrderProductionFailure(caught);
                 }
             });
