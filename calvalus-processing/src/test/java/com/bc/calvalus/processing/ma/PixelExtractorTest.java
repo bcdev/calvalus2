62b2f71fcd756e873997278c70fad27d06976f6a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-09-14 13:07:03 +0200

    Rem unused code

--- calvalus-processing/src/test/java/com/bc/calvalus/processing/ma/PixelExtractorTest.java
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/ma/PixelExtractorTest.java
@@ -14,17 +14,4 @@ public class PixelExtractorTest {
         PixelExtractor extractor = new PixelExtractor(new DefaultHeader(), new Product("A", "B", 2, 2), 1, null, null, true);
         extractor.extract(null);
     }
-
-    @Test
-    public void testHeaderNameReplacement() {
-        // 1. After aggregation, a single name "chl" shall generate 3 new names: "chl.mean", "chl.sigma", "chl.n"
-        //    Actually these could be expressions that are evaluated in the context of the record.
-
-        // String headerName = "chl-->chl.mean;chl.sigma;chl.n";
-
-        // 2. Or we simply expand header names marked with, e.g. '@': "@chl" would
-        //    then always produce 3 new names: "chl.mean", "chl.sigma", "chl.n"
-
-        // String headerName = "@chl";
-    }
 }
