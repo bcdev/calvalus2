43d864743d85cda10d006fd41ff89a0997caf096
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-09-16 11:34:16 +0200

    ?

--- calvalus-portal/src/main/java/com/bc/calvalus/portal/client/OrderProductionView.java
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/client/OrderProductionView.java
@@ -4,7 +4,6 @@ import com.bc.calvalus.portal.shared.DtoProductionRequest;
 import com.bc.calvalus.portal.shared.DtoProductionResponse;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
 
