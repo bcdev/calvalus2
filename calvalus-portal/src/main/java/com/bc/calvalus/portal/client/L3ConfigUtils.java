487e17fb27bbdc2b76f6deab28126dc607aa481a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-10-26 18:31:06 +0200

    Added test for period count computation

---
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/client/L3ConfigUtils.java
@@ -0,0 +1,42 @@
+package com.bc.calvalus.portal.client;
+
+import com.google.gwt.maps.client.geom.LatLngBounds;
+
+import java.util.Date;
+
+import static java.lang.Math.PI;
+
+/**
+ * Utility functions used by L3Config.
+ *
+ * @author Norman
+ */
+public class L3ConfigUtils  {
+
+    public static int getPeriodCount(Date minDate, Date maxDate, int steppingPeriodLengthValue, int compositingPeriodLengthValue) {
+        long millisPerDay = 24L * 60L * 60L * 1000L;
+        long deltaMillis = maxDate.getTime() - minDate.getTime() + millisPerDay;
+        int deltaDays = (int) (deltaMillis / millisPerDay);
+        int value = deltaDays / steppingPeriodLengthValue;
+        int rest = deltaDays % steppingPeriodLengthValue;
+        if (compositingPeriodLengthValue <= rest) {
+            value++;
+        }
+        return value;
+    }
+
+    public static int[] getTargetSizeEstimation(LatLngBounds regionBounds, double res) {
+        return getTargetSizeEstimation(regionBounds.getNorthEast().getLatitude() - regionBounds.getSouthWest().getLatitude(),
+                                       regionBounds.getNorthEast().getLongitude() - regionBounds.getSouthWest().getLongitude(),
+                                       res);
+
+    }
+
+    static int[] getTargetSizeEstimation(double deltaLat, double deltaLon, double res) {
+        // see: SeaWiFS Technical Report Series Vol. 32;
+        final double RE = 6378.145;
+        int width = 1 + (int) Math.round((RE * PI * deltaLon / 180.0) / res);
+        int height = 1 + (int) Math.round((RE * PI * deltaLat / 180.0) / res);
+        return new int[]{width, height};
+    }
+}
---
+++ calvalus-portal/src/test/java/com/bc/calvalus/portal/client/L3ConfigUtilsTest.java
@@ -0,0 +1,43 @@
+package com.bc.calvalus.portal.client;
+
+import junit.framework.TestCase;
+
+import java.util.Date;
+
+import static com.bc.calvalus.portal.client.L3ConfigUtils.getPeriodCount;
+
+public class L3ConfigUtilsTest  extends TestCase {
+    public void testPeriodCount() throws Exception {
+        assertEquals(1, getPeriodCount(new Date(2008, 5, 1),
+                                       new Date(2008, 5, 10),
+                                       30, 10));
+
+        assertEquals(0, getPeriodCount(new Date(2008, 5, 1),
+                                       new Date(2008, 5, 10),
+                                       30, 30));
+
+        assertEquals(1, getPeriodCount(new Date(2008, 5, 1),
+                                       new Date(2008, 5, 30),
+                                       30, 10));
+
+        assertEquals(2, getPeriodCount(new Date(2008, 5, 1),
+                                       new Date(2008, 6, 30),
+                                       30, 10));
+
+        assertEquals(3, getPeriodCount(new Date(2008, 5, 1),
+                                       new Date(2008, 7, 15),
+                                       30, 10));
+
+        assertEquals(3, getPeriodCount(new Date(2008, 5, 1),
+                                       new Date(2008, 7, 30),
+                                       30, 10));
+    }
+
+    public void testTargetSize() throws Exception {
+        int[] targetSize;
+
+        targetSize = L3ConfigUtils.getTargetSizeEstimation(180, 360, 9.28);
+        assertEquals(4319, targetSize[0]);
+        assertEquals(2160, targetSize[1]);
+    }
+}
