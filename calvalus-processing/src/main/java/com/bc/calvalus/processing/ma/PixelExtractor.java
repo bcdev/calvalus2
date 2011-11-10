a62badf75609dc2ca8be604114eddc448b25de1d
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2011-11-10 16:43:28 +0100

    maxTimeDifference must be > 0

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/ma/PixelExtractor.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/ma/PixelExtractor.java
@@ -293,7 +293,7 @@ public class PixelExtractor {
     }
 
     private boolean testTime() {
-        return maxTimeDifference != 0 && pixelTimeProvider != null;
+        return maxTimeDifference > 0 && pixelTimeProvider != null;
     }
 
     private PixelPos getSpatiallyValidPixelPos(Record referenceRecord) {
