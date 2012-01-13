1689fdc78ca8b516fa8638d54907670fcf7d5a92
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2012-01-13 11:28:22 +0100

    increase version for portal

--- calvalus-portal/src/main/java/com/bc/calvalus/portal/server/BackendServiceImpl.java
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/server/BackendServiceImpl.java
@@ -30,7 +30,7 @@ import java.util.logging.Logger;
  */
 public class BackendServiceImpl extends RemoteServiceServlet implements BackendService {
 
-    public static final String VERSION = "Calvalus version 1.1 (built on 2011-11-22)";
+    public static final String VERSION = "Calvalus version 1.2 (built on 2012-01-12)";
 
     private static final int PRODUCTION_STATUS_OBSERVATION_PERIOD = 2000;
 
