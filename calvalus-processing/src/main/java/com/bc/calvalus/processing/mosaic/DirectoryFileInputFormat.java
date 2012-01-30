fd5db04a605a212a434ab780c9923715b1ea4df6
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2012-01-30 16:09:05 +0100

    allow for empty directories in directory input format

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/mosaic/DirectoryFileInputFormat.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/mosaic/DirectoryFileInputFormat.java
@@ -21,6 +21,7 @@ import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.JobContext;
@@ -28,15 +29,18 @@ import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.security.TokenCache;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * A input format that generate a input for each file in the given directory/directories.
  */
 public class DirectoryFileInputFormat extends FileInputFormat {
+
 
     /**
      * Generate the list of files and make them into FileSplits.
@@ -56,10 +60,79 @@ public class DirectoryFileInputFormat extends FileInputFormat {
     }
 
     /**
+     * taken from "FileInputFormat" but ignore empty entries
+     */
+    protected List<FileStatus> listStatus(JobContext job) throws IOException {
+        List<FileStatus> result = new ArrayList<FileStatus>();
+        Path[] dirs = getInputPaths(job);
+        if (dirs.length == 0) {
+            throw new IOException("No input paths specified in job");
+        }
+
+        // get tokens for all the required FileSystems..
+        TokenCache.obtainTokensForNamenodes(job.getCredentials(), dirs, job.getConfiguration());
+
+        // creates a MultiPathFilter with the hiddenFileFilter and the
+        // user provided one (if any).
+        List<PathFilter> filters = new ArrayList<PathFilter>();
+        filters.add(hiddenFileFilter);
+        PathFilter jobFilter = getInputPathFilter(job);
+        if (jobFilter != null) {
+            filters.add(jobFilter);
+        }
+        PathFilter inputFilter = new MultiPathFilter(filters);
+
+        for (Path p : dirs) {
+            FileSystem fs = p.getFileSystem(job.getConfiguration());
+            FileStatus[] matches = fs.globStatus(p, inputFilter);
+            if (matches != null && matches.length > 0) {
+                for (FileStatus globStat : matches) {
+                    if (globStat.isDir()) {
+                        Collections.addAll(result, fs.listStatus(globStat.getPath(), inputFilter));
+                    } else {
+                        result.add(globStat);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
      * Creates a {@link com.bc.calvalus.processing.hadoop.NoRecordReader} because records are not used with this input format.
      */
     @Override
     public RecordReader<NullWritable, NullWritable> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
         return new NoRecordReader();
     }
+
+    private static final PathFilter hiddenFileFilter = new PathFilter() {
+        public boolean accept(Path p) {
+            String name = p.getName();
+            return !name.startsWith("_") && !name.startsWith(".");
+        }
+    };
+
+    /**
+     * Proxy PathFilter that accepts a path only if all filters given in the
+     * constructor do. Used by the listPaths() to apply the built-in
+     * hiddenFileFilter together with a user provided one (if any).
+     */
+    private static class MultiPathFilter implements PathFilter {
+        private List<PathFilter> filters;
+
+        public MultiPathFilter(List<PathFilter> filters) {
+            this.filters = filters;
+        }
+
+        public boolean accept(Path path) {
+            for (PathFilter filter : filters) {
+                if (!filter.accept(path)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 }
