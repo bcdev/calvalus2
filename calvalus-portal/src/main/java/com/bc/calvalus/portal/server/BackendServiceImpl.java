367a43c9cddd1c5caf1bb07276c8087cd5298af4
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2012-02-28 16:46:42 +0100

    updated version before redeploying

--- calvalus-portal/src/main/java/com/bc/calvalus/portal/server/BackendServiceImpl.java
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/server/BackendServiceImpl.java
@@ -30,7 +30,7 @@ import java.util.logging.Logger;
  */
 public class BackendServiceImpl extends RemoteServiceServlet implements BackendService {
 
-    public static final String VERSION = "Calvalus version 1.2 (built on 2012-01-12)";
+    public static final String VERSION = "Calvalus version 1.2 (built on 2012-02-28)";
 
     private static final int PRODUCTION_STATUS_OBSERVATION_PERIOD = 2000;
 
