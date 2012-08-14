c8db803ac0dd86a6e6cb4e26ace562260954b8af
martin-boettcher <martin.boettcher@brockmann-consult.de>
2012-08-14 17:11:26 +0200

    version updated to 1.4.1 for LC output formatting option

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/hadoop/HadoopProcessingService.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/hadoop/HadoopProcessingService.java
@@ -31,7 +31,7 @@ import java.util.logging.Logger;
 public class HadoopProcessingService implements ProcessingService<JobID> {
 
     public static final String CALVALUS_SOFTWARE_PATH = "/calvalus/software/0.5";
-    public static final String DEFAULT_CALVALUS_BUNDLE = "calvalus-1.4-SNAPSHOT";
+    public static final String DEFAULT_CALVALUS_BUNDLE = "calvalus-1.4.1-SNAPSHOT";
     public static final String DEFAULT_BEAM_BUNDLE = "beam-4.10-SNAPSHOT";
 
     private static final boolean DEBUG = Boolean.getBoolean("calvalus.debug");
