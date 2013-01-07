d287edf53c9b52923d7ad1973595af641a479c57
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2013-01-07 11:50:17 +0100

    add MEGS and l2gen shell Adapter

---
+++ calvalus-shell-executables/megs/megs-reportprogress.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#status comment 3073 15041 1057 1121
+function handle_progress() {
+  line=$1
+  echo $line
+  if [[ ${line} =~ status\ comment\ ([0-9]+)\ ([0-9]+)\ ([0-9]+)\ ([0-9]+) ]]; then
+    a1=${BASH_REMATCH[1]}
+    a2=${BASH_REMATCH[2]}
+    progress=$(echo "scale=3; ${a1} / ${a2}" | bc)
+    printf "CALVALUS_PROGRESS %.3f\n" $progress
+  fi
+}
+
+while true; do
+  sleep 60
+  handle_progress "$(tail -n 5 status.txt|grep comment)"
+done
+ 
