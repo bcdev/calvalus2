ac651f6808cd094c7318da5ee8b64ff34d10e090
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2013-02-06 11:54:22 +0100

    use defaults for min_width and min_height

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/BeamProcessorAdapter.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/BeamProcessorAdapter.java
@@ -52,8 +52,8 @@ public class BeamProcessorAdapter extends SubsetProcessorAdapter {
         String processorParameters = conf.get(JobConfigNames.CALVALUS_L2_PARAMETERS);
 
         Product subsetProduct = createSubset();
-        int minWidth = Integer.parseInt(conf.get(JobConfigNames.CALVALUS_INPUT_MIN_WIDTH));
-        int minHeight = Integer.parseInt(conf.get(JobConfigNames.CALVALUS_INPUT_MIN_HEIGHT));
+        int minWidth = conf.getInt(JobConfigNames.CALVALUS_INPUT_MIN_WIDTH, 0);
+        int minHeight = conf.getInt(JobConfigNames.CALVALUS_INPUT_MIN_HEIGHT, 0);
         if (subsetProduct.getSceneRasterWidth() < minWidth &&
             subsetProduct.getSceneRasterHeight() < minHeight) {
             String msgPattern = "The size of the intersection of the product with the region is very small [%d, %d]." +
