b2962fde3b6f42b3c405a50b1aac45d8be5bde63
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2013-06-21 12:06:09 +0200

    modified way to get diagnostic
    try to get only diagnostic from failed tasks, not from failed task attempts that recovered later on

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/hadoop/HadoopWorkflowItem.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/hadoop/HadoopWorkflowItem.java
@@ -26,7 +26,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.RunningJob;
-import org.apache.hadoop.mapred.TaskCompletionEvent;
+import org.apache.hadoop.mapred.TIPStatus;
+import org.apache.hadoop.mapred.TaskReport;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobID;
 
@@ -116,32 +117,53 @@ public abstract class HadoopWorkflowItem extends AbstractWorkflowItem {
             if (runningJob == null) {
                 return null;
             }
-            int eventCounter = 0;
-            while (true) {
-                TaskCompletionEvent[] taskCompletionEvents = runningJob.getTaskCompletionEvents(eventCounter);
-                if (taskCompletionEvents.length == 0) {
-                    break;
-                }
-                eventCounter += taskCompletionEvents.length;
-                for (TaskCompletionEvent taskCompletionEvent : taskCompletionEvents) {
-                    if (taskCompletionEvent.getTaskStatus().equals(TaskCompletionEvent.Status.FAILED)) {
-                        String[] taskDiagnostics = runningJob.getTaskDiagnostics(taskCompletionEvent.getTaskAttemptId());
-                        if (taskDiagnostics.length > 0) {
-                            // this is a stack trace and we only want the case
-                            // so take the first line
-                            // remove the "*Exception: " prefixes (can be multiple)
-                            String firstMessage = taskDiagnostics[0];
-                            String firstLine = firstMessage.split("\\n")[0];
-                            String[] firstLineSplit = firstLine.split("Exception: ");
-                            return firstLineSplit[firstLineSplit.length -1];
-                        }
+            for (TaskReport mapTaskReport : jobClient.getMapTaskReports(downgradeJobId)) {
+                if (mapTaskReport.getCurrentStatus().equals(TIPStatus.FAILED)) {
+                    String[] taskDiagnostics = mapTaskReport.getDiagnostics();
+                    if (taskDiagnostics.length > 0) {
+                        return getErrorMessageFromDiagnostics(taskDiagnostics);
                     }
                 }
             }
+            for (TaskReport reduceTaskReport : jobClient.getReduceTaskReports(downgradeJobId)) {
+                if (reduceTaskReport.getCurrentStatus().equals(TIPStatus.FAILED)) {
+                    String[] taskDiagnostics = reduceTaskReport.getDiagnostics();
+                    if (taskDiagnostics.length > 0) {
+                        return getErrorMessageFromDiagnostics(taskDiagnostics);
+                    }
+                }
+            }
+
+//            int eventCounter = 0;
+//            while (true) {
+//                TaskCompletionEvent[] taskCompletionEvents = runningJob.getTaskCompletionEvents(eventCounter);
+//                if (taskCompletionEvents.length == 0) {
+//                    break;
+//                }
+//                eventCounter += taskCompletionEvents.length;
+//                for (TaskCompletionEvent taskCompletionEvent : taskCompletionEvents) {
+//                    if (taskCompletionEvent.getTaskStatus().equals(TaskCompletionEvent.Status.FAILED)) {
+//                        String[] taskDiagnostics = runningJob.getTaskDiagnostics(taskCompletionEvent.getTaskAttemptId());
+//                        if (taskDiagnostics.length > 0) {
+//                            return getErrorMessageFromDiagnostics(taskDiagnostics);
+//                        }
+//                    }
+//                }
+//            }
         } catch (IOException e) {
             return null;
         }
         return null;
+    }
+
+    static String getErrorMessageFromDiagnostics(String[] taskDiagnostics) {
+        // this is a stack trace and we only want the case
+        // so take the first line
+        // remove the "*Exception: " prefixes (can be multiple)
+        String firstMessage = taskDiagnostics[0];
+        String firstLine = firstMessage.split("\\n")[0];
+        String[] firstLineSplit = firstLine.split("Exception: ");
+        return firstLineSplit[firstLineSplit.length - 1];
     }
 
     @Override
