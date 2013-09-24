3ce7574a2f687b43e2d4d120041595c1e9fa2322
marpet <marco.peters@brockmann-consult.de>
2013-09-24 10:11:22 +0200

    added workaround for GWT Bug 7525

---
+++ calvalus-portal/src/main/java/com/google/gwt/core/client/GWTBridge.java
@@ -0,0 +1,12 @@
+package com.google.gwt.core.client;
+
+/**
+ * This class provides a workarounf for the GWT Bug 7527 (http://code.google.com/p/google-web-toolkit/issues/detail?id=7527)
+ * <p/>
+ * The solution is taken from http://alexluca.com/2013/01/17/gwt-25-and-extgwt-224-classnotfoundexception-comgooglegwtcoreclientgwtbridge/
+ * <p/>
+ * This should be fixed with GWT release 2.5.2, hopefully.
+ */
+public abstract class GWTBridge extends com.google.gwt.core.shared.GWTBridge {
+
+}
