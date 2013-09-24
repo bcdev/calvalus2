c5b4e2c6a982eccc672e811820dd39566a7e1085
marpet <marco.peters@brockmann-consult.de>
2013-09-24 10:12:00 +0200

    typo

--- calvalus-portal/src/main/java/com/google/gwt/core/client/GWTBridge.java
+++ calvalus-portal/src/main/java/com/google/gwt/core/client/GWTBridge.java
@@ -1,7 +1,7 @@
 package com.google.gwt.core.client;
 
 /**
- * This class provides a workarounf for the GWT Bug 7527 (http://code.google.com/p/google-web-toolkit/issues/detail?id=7527)
+ * This class provides a workaround for the GWT Bug 7527 (http://code.google.com/p/google-web-toolkit/issues/detail?id=7527)
  * <p/>
  * The solution is taken from http://alexluca.com/2013/01/17/gwt-25-and-extgwt-224-classnotfoundexception-comgooglegwtcoreclientgwtbridge/
  * <p/>
