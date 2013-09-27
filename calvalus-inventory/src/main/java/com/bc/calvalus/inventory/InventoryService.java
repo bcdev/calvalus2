b01f9a746fb5da4baaa69a9b026f7bfacae2aedc
marpet <marco.peters@brockmann-consult.de>
2013-09-27 09:44:01 +0200

    added API doc

--- calvalus-inventory/src/main/java/com/bc/calvalus/inventory/InventoryService.java
+++ calvalus-inventory/src/main/java/com/bc/calvalus/inventory/InventoryService.java
@@ -16,7 +16,9 @@ public interface InventoryService {
      * Gets pre-defined product sets.
      *
      * @param filter A filter expression (unused)
+     *
      * @return The array product sets, which may be empty.
+     *
      * @throws java.io.IOException If an I/O error occurs
      */
     ProductSet[] getProductSets(String filter) throws Exception;
@@ -27,18 +29,39 @@ public interface InventoryService {
      * <i>TODO: Use Unix file system wildcards instead (nf, 2011-09-09). See {@link AbstractInventoryService#getRegexpForPathGlob(String)}. </i>
      *
      * @param pathPatterns A list of relative or absolute data paths which may contain regular expressions.
+     *
      * @return An array of fully qualified URIs comprising the filesystem and absolute data input path.
-     * @throws java.io.IOException If an I/O error occurs.
+     *
+     * @throws java.io.IOException If an I/O error occurs
      */
     String[] globPaths(List<String> pathPatterns) throws IOException;
 
     /**
      * @param path A relative or absolute path.
+     *
      * @return A fully qualified URI comprising the filesystem and absolute data output path.
      */
     String getQualifiedPath(String path);
 
-    OutputStream addFile(String userPath)throws IOException;
+    /**
+     * Adds a file to the inventory (creates necessary directories) and returns an {@link OutputStream output stream} for writing data into it.
+     *
+     * @param userPath the path to the file to be created.
+     *
+     * @return the {@link OutputStream output stream} for writing.
+     *
+     * @throws IOException If an I/O error occurs
+     */
+    OutputStream addFile(String userPath) throws IOException;
 
-    boolean removeFile(String userPath)throws IOException;
+    /**
+     * Deletes the file specified by the given path.
+     *
+     * @param userPath the path to the file to be deleted.
+     *
+     * @return {@code true}, if the deletion was successful, otherwise {@code false}.
+     *
+     * @throws IOException If an I/O error occurs
+     */
+    boolean removeFile(String userPath) throws IOException;
 }
