72beeae492a6bb5bead5ec07b3e10475e8bb59bc
marpet <marco.peters@brockmann-consult.de>
2013-11-15 17:58:50 +0100

    ignore class in tests

--- calvalus-processing/src/test/java/com/bc/calvalus/processing/ma/TestHeader.java
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/ma/TestHeader.java
@@ -1,10 +1,13 @@
 package com.bc.calvalus.processing.ma;
 
+import org.junit.Ignore;
+
 /**
  * Provides easy constructors for the test cases
  *
  * @author Marco Peters
  */
+@Ignore
 public class TestHeader extends DefaultHeader {
 
     public TestHeader(String... attributeNames) {
