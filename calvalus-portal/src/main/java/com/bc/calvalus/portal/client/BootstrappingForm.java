2f1b9b794456dcf728959169f172ef68f89ace0d
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2014-04-02 11:31:01 +0200

    add processor parameters UI onto bootstrapping  tab

--- calvalus-portal/src/main/java/com/bc/calvalus/portal/client/BootstrappingForm.java
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/client/BootstrappingForm.java
@@ -1,12 +1,8 @@
 package com.bc.calvalus.portal.client;
 
-import com.bc.calvalus.commons.shared.BundleFilter;
 import com.bc.calvalus.portal.shared.DtoProcessorDescriptor;
 import com.bc.calvalus.processing.boostrapping.BootstrappingWorkflowItem;
-import com.bc.calvalus.production.hadoop.ProcessorProductionRequest;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Document;
-import com.google.gwt.event.dom.client.DomEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
@@ -17,11 +13,7 @@ import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,8 +23,6 @@ public class BootstrappingForm extends Composite {
 
     private static final int DEFAULT_NUMBER_OF_ITERATIONS = 10000;
     private final UserManagedFiles userManagedContent;
-    private final PortalContext portalContext;
-
 
     interface TheUiBinder extends UiBinder<Widget, BootstrappingForm> {
 
@@ -47,21 +37,18 @@ public class BootstrappingForm extends Composite {
     @UiField
     Button removeBootstrapSourceButton;
 
-    @UiField
-    ListBox processorList;
+    @UiField(provided = true)
+    L2ConfigForm l2ConfigForm;
     @UiField
     IntegerBox numberOfIterations;
     @UiField
     TextBox productionName;
 
-    private final List<DtoProcessorDescriptor> processorDescriptors;
-
-
     public BootstrappingForm(PortalContext portalContext) {
-        this.portalContext = portalContext;
+        l2ConfigForm = new L2ConfigForm(portalContext, new BootstrappingFilter(), true);
+        l2ConfigForm.processorListLabel.setText("Processor");
+        l2ConfigForm.parametersLabel.setText("Parameters");
         initWidget(uiBinder.createAndBindUi(this));
-
-        processorDescriptors = new ArrayList<DtoProcessorDescriptor>();
 
         numberOfIterations.setValue(DEFAULT_NUMBER_OF_ITERATIONS);
 
@@ -78,11 +65,9 @@ public class BootstrappingForm extends Composite {
         userManagedContent.updateList();
 
         //TODO filter bootstrap processor(s) in all other views
-        updateProcessorList();
     }
 
     public void validateForm() throws ValidationException {
-
         Integer numberOfIterationsValue = numberOfIterations.getValue();
         if (numberOfIterationsValue == null || numberOfIterationsValue <= 0) {
             throw new ValidationException(numberOfIterations, "Number of Iterations must be > 0");
@@ -92,19 +77,12 @@ public class BootstrappingForm extends Composite {
         if (!bootstrapSourceValid) {
             throw new ValidationException(bootstrapSources, "Bootstrap source must be given.");
         }
-        boolean processorDescriptorValid = getSelectedProcessorDescriptor() != null;
-        if (!processorDescriptorValid) {
-            throw new ValidationException(this, "No Bootstrapping processor selected.");
-        }
+        l2ConfigForm.validateForm();
     }
 
     public Map<String, String> getValueMap() {
         Map<String, String> parameters = new HashMap<String, String>();
-        DtoProcessorDescriptor processorDescriptor = getSelectedProcessorDescriptor();
-        parameters.put(ProcessorProductionRequest.PROCESSOR_BUNDLE_NAME, processorDescriptor.getBundleName());
-        parameters.put(ProcessorProductionRequest.PROCESSOR_BUNDLE_VERSION, processorDescriptor.getBundleVersion());
-        parameters.put(ProcessorProductionRequest.PROCESSOR_BUNDLE_LOCATION, processorDescriptor.getBundleLocation());
-        parameters.put(ProcessorProductionRequest.PROCESSOR_NAME, processorDescriptor.getExecutableName());
+        parameters.putAll(l2ConfigForm.getValueMap());
 
         parameters.put(BootstrappingWorkflowItem.NUM_ITERATIONS_PROPERTY, numberOfIterations.getText());
         parameters.put(BootstrappingWorkflowItem.INPUT_FILE_PROPRTY, userManagedContent.getSelectedFilePath());
@@ -112,50 +90,14 @@ public class BootstrappingForm extends Composite {
             parameters.put("productionName", productionName.getValue());
         }
         parameters.put("autoStaging", String.valueOf(true));
-
-
         return parameters;
     }
 
-    public void updateProcessorList() {
-        DtoProcessorDescriptor oldSelection = getSelectedProcessorDescriptor();
-
-        processorDescriptors.clear();
-        Collections.addAll(processorDescriptors, portalContext.getProcessors(BundleFilter.PROVIDER_USER));
-
-        final Iterator<DtoProcessorDescriptor> iterator = processorDescriptors.iterator();
-        while (iterator.hasNext()) {
-            DtoProcessorDescriptor processorDescriptor = iterator.next();
-            boolean isBootstrapProcessor = "r-bootstrap".equals(processorDescriptor.getExecutableName());
-            if (!isBootstrapProcessor || processorDescriptor.isL2Processor()) {
-                iterator.remove();
-            }
-        }
-
-        processorList.clear();
-        int newSelectionIndex = 0;
-        boolean productSetChanged = true;
-        for (DtoProcessorDescriptor processor : processorDescriptors) {
-            String label = processor.getProcessorName() + " v" + processor.getProcessorVersion();
-            processorList.addItem(label);
-            if (oldSelection != null && oldSelection.equals(processor)) {
-                newSelectionIndex = processorList.getItemCount() - 1;
-                productSetChanged = false;
-            }
-        }
-        processorList.setSelectedIndex(newSelectionIndex);
-        if (productSetChanged) {
-            DomEvent.fireNativeEvent(Document.get().createChangeEvent(), processorList);
+    private static class BootstrappingFilter implements Filter<DtoProcessorDescriptor> {
+        @Override
+        public boolean accept(DtoProcessorDescriptor dtoProcessorDescriptor) {
+            boolean isBootstrapProcessor = "r-bootstrap".equals(dtoProcessorDescriptor.getExecutableName());
+            return isBootstrapProcessor && !dtoProcessorDescriptor.isL2Processor();
         }
     }
-
-    public DtoProcessorDescriptor getSelectedProcessorDescriptor() {
-        int selectedIndex = processorList.getSelectedIndex();
-        if (selectedIndex >= 0) {
-            return processorDescriptors.get(selectedIndex);
-        } else {
-            return null;
-        }
-    }
-
 }
