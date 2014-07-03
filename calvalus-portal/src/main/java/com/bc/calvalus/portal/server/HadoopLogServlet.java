1a109d9cbe3d285e6f78bfc57b6f2e3eda2b9855
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2014-07-03 17:31:56 +0200

    fix log file display

--- calvalus-portal/src/main/java/com/bc/calvalus/portal/server/HadoopLogServlet.java
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/server/HadoopLogServlet.java
@@ -24,20 +24,35 @@ import com.bc.calvalus.production.Production;
 import com.bc.calvalus.production.ProductionException;
 import com.bc.calvalus.production.ProductionService;
 import com.bc.calvalus.production.store.ProxyWorkflow;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileContext;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.RunningJob;
 import org.apache.hadoop.mapred.TaskAttemptID;
 import org.apache.hadoop.mapred.TaskCompletionEvent;
 import org.apache.hadoop.mapreduce.JobID;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.logaggregation.AggregatedLogFormat;
+import org.apache.hadoop.yarn.logaggregation.LogAggregationUtils;
+import org.apache.hadoop.yarn.util.ConverterUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.URL;
 
@@ -101,21 +116,21 @@ public class HadoopLogServlet extends HttpServlet {
 
             HadoopProcessingService processingService = hadoopWorkflowItem.getProcessingService();
             JobClient jobClient = processingService.getJobClient(userName);
-            showLogFor(resp, workflowItem, jobClient, status);
+            showLogFor(resp, workflowItem, jobClient, status, userName);
         } else if (workflowItem instanceof ProxyWorkflow) {
             ProxyWorkflow proxyWorkflow = (ProxyWorkflow) workflowItem;
 
             HadoopProcessingService processingService = (HadoopProcessingService) proxyWorkflow.getProcessingService();
             JobClient jobClient = processingService.getJobClient(userName);
-            showLogFor(resp, workflowItem, jobClient, TaskCompletionEvent.Status.FAILED);
+            showLogFor(resp, workflowItem, jobClient, status, userName);
         } else {
             showErrorPage("Not able to provide logs.", resp);
         }
     }
 
-    private void showLogFor(HttpServletResponse resp, WorkflowItem hadoopWorkflowItem, JobClient jobClient, TaskCompletionEvent.Status status) throws IOException {
+    private void showLogFor(HttpServletResponse resp, WorkflowItem hadoopWorkflowItem, JobClient jobClient, TaskCompletionEvent.Status status, String userName) throws IOException {
         Object[] jobIds = hadoopWorkflowItem.getJobIds();
-        if(jobIds.length != 1) {
+        if (jobIds.length != 1) {
             showErrorPage("found not one jobId, but:" + jobIds.length, resp);
             return;
         }
@@ -123,6 +138,7 @@ public class HadoopLogServlet extends HttpServlet {
         org.apache.hadoop.mapred.JobID downgradeJobId = org.apache.hadoop.mapred.JobID.downgrade(jobId);
         try {
             RunningJob runningJob = jobClient.getJob(downgradeJobId);
+            Configuration conf = jobClient.getConf();
             if (runningJob == null) {
                 showErrorPage("No 'RunningJob' found for jobId: " + jobId, resp);
                 return;
@@ -138,12 +154,29 @@ public class HadoopLogServlet extends HttpServlet {
                     if (event.getTaskStatus().equals(status)) {
                         // TaskID taskID = event.getTaskAttemptId().getTaskID();
                         // TODO in case of status == failed --> check if task has really failed.
-                        displayTaskLogs(event.getTaskAttemptId(), event.getTaskTrackerHttp(), resp);
+//                        displayTaskLogs(event.getTaskAttemptId(), event.getTaskTrackerHttp(), resp);
+
+
+                        String appOwner = userName;
+                        StringBuilder appSB = new StringBuilder("application");
+                        String appIdStr = jobId.appendTo(appSB).toString();
+
+                        ApplicationId appId = null;
+                        try {
+                            appId = ConverterUtils.toApplicationId(appIdStr);
+                        } catch (Exception e) {
+                            System.err.println("Invalid ApplicationId specified");
+                            return;
+                        }
+                        int resultCode = dumpAllContainersLogs(appId, appOwner, resp.getOutputStream(), conf);
+                        if (resultCode != 0) {
+                            showErrorPage("Failed to open Logfile.", resp);
+                        }
                         return;
                     }
                 }
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             showErrorPage("I/O error", resp);
         }
     }
@@ -199,13 +232,68 @@ public class HadoopLogServlet extends HttpServlet {
         writer.println("<body>");
         writer.println("<h1>Error while showing processing log</h1>");
         writer.println("While retrieving the logfile from the Hadoop Cluster an error occurred.</br>" +
-                    "In most cases this is related to the fact, that the logs are automatically removed after roughly 24 hours.</br>" +
-                    "Depending on the cluster activity this can happen sooner or later.</br></br></br>");
+                       "In most cases this is related to the fact, that the logs are automatically removed after roughly 24 hours.</br>" +
+                       "Depending on the cluster activity this can happen sooner or later.</br></br></br>");
         writer.println("<hr>");
         writer.println("<h5>Internal error message</h5>");
         writer.println(message);
         writer.println("</body>");
         writer.flush();
         writer.close();
+    }
+
+
+    public int dumpAllContainersLogs(ApplicationId appId, String appOwner,
+                                     OutputStream outputStream, Configuration conf) throws IOException {
+        Path remoteRootLogDir = new Path(conf.get(
+                YarnConfiguration.NM_REMOTE_APP_LOG_DIR,
+                YarnConfiguration.DEFAULT_NM_REMOTE_APP_LOG_DIR));
+        String user = appOwner;
+        String logDirSuffix = LogAggregationUtils.getRemoteNodeLogDirSuffix(conf);
+        // TODO Change this to get a list of files from the LAS.
+        Path remoteAppLogDir = LogAggregationUtils.getRemoteAppLogDir(
+                remoteRootLogDir, appId, user, logDirSuffix);
+        RemoteIterator<FileStatus> nodeFiles;
+        try {
+            nodeFiles = FileContext.getFileContext(conf).listStatus(remoteAppLogDir);
+        } catch (FileNotFoundException fnf) {
+            System.out.println("Logs not available at " + remoteAppLogDir.toString());
+            System.out
+                    .println("Log aggregation has not completed or is not enabled.");
+            return -1;
+        }
+        while (nodeFiles.hasNext()) {
+            FileStatus thisNodeFile = nodeFiles.next();
+            AggregatedLogFormat.LogReader reader = new AggregatedLogFormat.LogReader(
+                    conf, new Path(remoteAppLogDir, thisNodeFile.getPath().getName()));
+            try {
+
+                DataInputStream valueStream;
+                AggregatedLogFormat.LogKey key = new AggregatedLogFormat.LogKey();
+                valueStream = reader.next(key);
+
+                PrintStream out = new PrintStream(outputStream);
+                while (valueStream != null) {
+                    String containerString = "\n\nContainer: " + key + " on "
+                                             + thisNodeFile.getPath().getName();
+                    out.println(containerString);
+                    out.println(StringUtils.repeat("=", containerString.length()));
+                    while (true) {
+                        try {
+                            AggregatedLogFormat.LogReader.readAContainerLogsForALogType(valueStream, out);
+                        } catch (EOFException eof) {
+                            break;
+                        }
+                    }
+
+                    // Next container
+                    key = new AggregatedLogFormat.LogKey();
+                    valueStream = reader.next(key);
+                }
+            } finally {
+                reader.close();
+            }
+        }
+        return 0;
     }
 }
