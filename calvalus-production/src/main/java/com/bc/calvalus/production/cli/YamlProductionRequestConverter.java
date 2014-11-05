b6bb5f1ed580427355465877b196ea2083bbf2ee
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2014-11-05 19:21:53 +0100

    add first version of yaml converter

---
+++ calvalus-production/src/main/java/com/bc/calvalus/production/cli/YamlProductionRequestConverter.java
@@ -0,0 +1,62 @@
+package com.bc.calvalus.production.cli;
+
+import com.bc.calvalus.production.ProductionRequest;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by marco on 05.11.14.
+ */
+public class YamlProductionRequestConverter {
+
+    private final Map<String, Object> map;
+
+    public YamlProductionRequestConverter(Reader reader) {
+        Yaml yaml = new Yaml();
+        map = (Map<String, Object>) yaml.load(reader);
+    }
+
+    public ProductionRequest loadProductionRequest(String userName) {
+        String productionType = (String) map.get("productionType");
+        Map<String, String> parameterMap = new HashMap<>();
+        Set<Map.Entry<String, Object>> entries = map.entrySet();
+        for (Map.Entry<String, Object> entry : entries) {
+            String key = entry.getKey();
+            if (!key.equals("productionType")) {
+                Object value = entry.getValue();
+                if (value instanceof String) {
+                    parameterMap.put(key, ((String) value).trim());
+                } else if (value instanceof String[]) {
+                    parameterMap.put(key, arrayToString((String[]) value));
+                } else if (value instanceof List) {
+                    List<String> sa = (List) value;
+                    parameterMap.put(key, arrayToString(sa.toArray(new String[sa.size()])));
+                } else if (value == null) {
+                    parameterMap.put(key, "");
+                } else {
+                    System.out.println("value = " + value);
+                    System.out.println("value.class = " + value.getClass());
+                }
+            }
+        }
+        return new ProductionRequest(productionType, userName, parameterMap);
+    }
+
+    static String arrayToString(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        StringBuilder sbuf = new StringBuilder();
+        sbuf.append(strs[0]);
+        for (int idx = 1; idx < strs.length; idx++) {
+            sbuf.append(",");
+            sbuf.append(strs[idx].trim());
+        }
+        return sbuf.toString();
+    }
+}
---
+++ calvalus-production/src/test/java/com/bc/calvalus/production/cli/YamlProductionRequestConverterTest.java
@@ -0,0 +1,60 @@
+package com.bc.calvalus.production.cli;
+
+import com.bc.calvalus.production.ProductionRequest;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class YamlProductionRequestConverterTest {
+
+    @Test
+    public void testConversionToMap() throws Exception {
+        InputStream resourceAsStream = getClass().getResourceAsStream("ma-request.yaml");
+        InputStreamReader inputStreamReader = new InputStreamReader(resourceAsStream);
+        ProductionRequest productionRequest = new YamlProductionRequestConverter(inputStreamReader).loadProductionRequest("testuser");
+
+        assertNotNull(productionRequest);
+        assertEquals("MA", productionRequest.getProductionType());
+        assertNotNull(productionRequest.getUserName());
+        assertEquals("testuser", productionRequest.getUserName());
+        Map<String, String> parameters = productionRequest.getParameters();
+        assertNotNull(parameters);
+        //assertEquals(10, parameters.size());
+
+        assertTrue(parameters.containsKey("calvalus.processor.package"));
+        assertEquals("beam-meris-radiometry", parameters.get("calvalus.processor.package"));
+
+        assertTrue(parameters.containsKey("calvalus.input"));
+
+        String inputValue = parameters.get("calvalus.input");
+        assertTrue(inputValue.contains(","));
+        String[] splits = inputValue.split(",");
+        assertEquals(2, splits.length);
+        assertEquals("hdfs://master00:9000/calvalus/eodata/MER_RR__1P/r03/2004/07/15/MER_RR__1PRACR20040715_011806_000026382028_00332_12410_0000.N1", splits[0]);
+        assertEquals("hdfs://master00:9000/calvalus/eodata/MER_RR__1P/r03/2004/07/15/MER_RR__1PRACR20040715_021806_000026382028_00332_12410_0000.N1", splits[1]);
+
+        assertTrue(parameters.containsKey("calvalus.l2.parameters"));
+        String l2Parameters = parameters.get("calvalus.l2.parameters");
+        assertTrue(l2Parameters.startsWith("<parameters>"));
+        assertTrue(l2Parameters.endsWith("</parameters>"));
+        assertTrue(l2Parameters.contains("<doSmile>true</doSmile>"));
+        assertTrue(l2Parameters.contains("<reproVersion>AUTO_DETECT</reproVersion>"));
+
+        assertTrue(parameters.containsKey("calvalus.output.compression"));
+        assertNotNull(parameters.get("calvalus.output.compression"));
+        assertEquals("", parameters.get("calvalus.output.compression"));
+
+        assertEquals("BEAM-DIMAP", parameters.get("calvalus.input.format"));
+
+        assertNull(productionRequest.getRegionGeometry(null));
+
+        assertNotNull(parameters.get("calvalus.plainText.parameter"));
+        String expectedPlainText = "<parameters>\nThis is a multiline\nTextfield\n</parameters>";
+        assertEquals(expectedPlainText, parameters.get("calvalus.plainText.parameter"));
+    }
+
+}
