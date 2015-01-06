401fdafd5fa528ac965b5289d17109c9482cb223
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2015-01-06 18:25:24 +0100

    reduce memory footprint

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/l3/multiband/L3MultiBandFormatMapper.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/l3/multiband/L3MultiBandFormatMapper.java
@@ -2,8 +2,6 @@ package com.bc.calvalus.processing.l3.multiband;
 
 import com.bc.calvalus.processing.l3.L3TemporalBin;
 import com.bc.calvalus.processing.l3.multiregion.L3MultiRegionBinIndex;
-import org.apache.hadoop.conf.Configurable;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -19,11 +17,13 @@ import java.io.IOException;
  */
 public class L3MultiBandFormatMapper extends Mapper<LongWritable, L3TemporalBin, L3MultiRegionBinIndex, FloatWritable> {
 
+    private final L3MultiRegionBinIndex key = new L3MultiRegionBinIndex(0, 0);
+    private final FloatWritable binValue = new FloatWritable();
+
     @Override
     protected void map(LongWritable binIndex, L3TemporalBin temporalBin, Context context) throws IOException, InterruptedException {
-        L3MultiRegionBinIndex key = new L3MultiRegionBinIndex(0, binIndex.get());
-        FloatWritable binValue = new FloatWritable();
         float[] temporalFeatureValues = temporalBin.getFeatureValues();
+        key.setBinIndex(binIndex.get());
         for (int i=0; i< temporalFeatureValues.length; ++i) {
             key.setRegionIndex(i);
             binValue.set(temporalFeatureValues[i]);
