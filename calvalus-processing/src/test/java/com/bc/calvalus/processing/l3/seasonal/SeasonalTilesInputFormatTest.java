de6b6f73eab288f60e7df5f13fe9f1e0823345c8
martin-boettcher <martin.boettcher@brockmann-consult.de>
2016-02-28 22:28:21 +0100

    seasonal compositing and climatology generation corrected to really use the input format implementation

--- calvalus-processing/src/test/java/com/bc/calvalus/processing/l3/seasonal/SeasonalTilesInputFormatTest.java
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/l3/seasonal/SeasonalTilesInputFormatTest.java
@@ -1,5 +1,6 @@
 package com.bc.calvalus.processing.l3.seasonal;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -8,6 +9,8 @@ import org.apache.hadoop.mapreduce.InputSplit;
 import org.junit.Test;
 import org.mockito.Mockito;
 import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +22,15 @@ import java.util.List;
 public class SeasonalTilesInputFormatTest {
     @Test
     public void testCreateSplits() throws Exception {
-        SeasonalTilesInputFormat inputFormat = new SeasonalTilesInputFormat();
-        FileSystem fs = Mockito.mock(FileSystem.class);
+        final FileSystem fs = Mockito.mock(FileSystem.class);
         Mockito.when(fs.getFileBlockLocations((FileStatus) Mockito.any(), Mockito.anyInt(), Mockito.anyInt())).
                 thenReturn(new BlockLocation[]{new BlockLocation(new String[]{"name"}, new String[]{"host"}, 0, 99)});
+        SeasonalTilesInputFormat inputFormat = new SeasonalTilesInputFormat() {
+            @Override
+            protected FileSystem getFileSystem(FileStatus file, Configuration conf) throws IOException {
+                return fs;
+            }
+        };
         FileStatus[] files = new FileStatus[] {
                 new FileStatus(99, false, 1, 99, 0, new Path("/ab/cd/ESACCI-LC-L3-SR-MERIS-300m-P7D-h36v08-20090108-v2.0.nc")),
                 new FileStatus(99, false, 1, 99, 0, new Path("/ab/cd/ESACCI-LC-L3-SR-MERIS-300m-P7D-h36v08-20090115-v2.0.nc")),
@@ -30,7 +38,7 @@ public class SeasonalTilesInputFormatTest {
         };
         List<InputSplit> splits = new ArrayList<InputSplit>();
 
-        inputFormat.createSplits(fs, null, files, splits);
+        inputFormat.createSplits(null, files, splits, new Configuration());
 
         assertEquals(2, splits.size());
     }
