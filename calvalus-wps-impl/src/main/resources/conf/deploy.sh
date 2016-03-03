468581faa968792aaabac3c6e61e33360f5646ba
Hans Permana <hans.permana@gmail.com>
2016-03-03 16:21:13 +0100

    Added an assembly package for the plugin deployment (with sh script).

---
+++ calvalus-wps-impl/src/main/resources/conf/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CATALINA_HOME=/opt/tomcat
+SERVICE_NAME=bc-tomcat
+RUN_AS=tomcat
+
+sudo service bc-tomcat stop
+sudo -u $RUN_AS mkdir -p $CATALINA_HOME/conf/calvalus
+sudo -u $RUN_AS cp calvalus.config $CATALINA_HOME/conf/calvalus
+sudo -u $RUN_AS cp log4j.properties $CATALINA_HOME/conf/calvalus
+sudo -u $RUN_AS cp xsd/calwpsL3Parameters-schema.xsd $CATALINA_HOME/webapps/bc-wps
+sudo -u $RUN_AS cp *.jar $CATALINA_HOME/webapps/bc-wps/WEB-INF/lib
+sudo service bc-tomcat start
