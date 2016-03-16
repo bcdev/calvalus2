4c5ca94e6bf40f56b3625424db522854260535b7
Hans Permana <hans.permana@gmail.com>
2016-03-16 11:44:39 +0100

    Added a wps client pack for testing. Modified the pom file and created a new assembly file accordingly.

---
+++ calvalus-wps-impl/src/main/resources/wps-client-subsetting/getCapabilities.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This is a script to send GetCapabilities request to Calvalus WPS. The response is displayed on the screen.
+# Usage : getCapabilities.sh
+# Example : getCapabilities.sh
+
+read -p "Enter User Name: " WPS_USER
+
+wget -q --user=$WPS_USER --ask-password -O- "www.brockmann-consult.de/bc-wps/wps/calvalus?Service=WPS&Request=GetCapabilities"
---
+++ calvalus-wps-impl/src/main/resources/wps-client-subsetting/getStatus.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This is a script to inquire a status of a process to Calvalus WPS with a given execute response XML file. The response of this request
+# will be displayed on the screen. When the process has been successfully completed, the result URL is also provided in the response.
+# Usage : getStatus.sh [execute response XML file]
+# Example : getStatus.sh response.xml
+
+RESPONSE_FILE=$1
+
+GET_STATUS_URL=`grep "statusLocation=" response.xml | cut -d'"' -f2`
+
+STATUS_URL_FORMATTED=`echo ${GET_STATUS_URL} | sed -e "s/\&amp;/\&/g"`
+
+read -p "Enter User Name: " WPS_USER
+
+wget -q --user=$WPS_USER --ask-password -O- "${STATUS_URL_FORMATTED}"
