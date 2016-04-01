cba030ec98770c19c85dcc28e3250737e95ca279
Hans Permana <hans.permana@gmail.com>
2016-04-01 13:40:21 +0200

    modified deploy.sh to include staging files.

--- calvalus-wps-impl/src/main/resources/conf/deploy.sh
+++ calvalus-wps-impl/src/main/resources/conf/deploy.sh
@@ -9,6 +9,9 @@ sudo -u $RUN_AS mkdir -p $CATALINA_HOME/conf/calvalus
 sudo -u $RUN_AS cp calvalus.config $CATALINA_HOME/conf/calvalus
 sudo -u $RUN_AS cp calvalus-wps.properties $CATALINA_HOME/conf/calvalus
 sudo -u $RUN_AS cp log4j.properties $CATALINA_HOME/conf/calvalus
+sudo -u $RUN_AS mkdir -p $CATALINA_HOME/webapps/bc-wps/staging
+sudo -u $RUN_AS cp staging/directory-listing.xsl $CATALINA_HOME/webapps/bc-wps/staging
+sudo -u $RUN_AS cp staging/directory-listing-readme.txt $CATALINA_HOME/webapps/bc-wps/staging
 sudo -u $RUN_AS cp xsd/calwpsL3Parameters-schema.xsd $CATALINA_HOME/webapps/bc-wps
 sudo -u $RUN_AS cp *.jar $CATALINA_HOME/webapps/bc-wps/WEB-INF/lib
 sudo service bc-tomcat start
