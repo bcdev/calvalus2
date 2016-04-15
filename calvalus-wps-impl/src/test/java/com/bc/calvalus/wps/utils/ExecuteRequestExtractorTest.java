f6d1a0761ae689e17870360e40d5191d5617d453
Hans Permana <hans.permana@gmail.com>
2016-04-15 11:44:23 +0200

    test modified due to the namespace change in the execute request.

--- calvalus-wps-impl/src/test/java/com/bc/calvalus/wps/utils/ExecuteRequestExtractorTest.java
+++ calvalus-wps-impl/src/test/java/com/bc/calvalus/wps/utils/ExecuteRequestExtractorTest.java
@@ -100,64 +100,66 @@ public class ExecuteRequestExtractorTest {
     private String getExecuteRequestString() {
         return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>\n" +
                "\n" +
-               "<wps:Execute service=\"WPS\"\n" +
+               "<wps:Execute xsi:schemaLocation=\"http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsExecute_request.xsd\"" +
+               "             service=\"WPS\"\n" +
                "             version=\"1.0.0\"\n" +
                "             xmlns:wps=\"http://www.opengis.net/wps/1.0.0\"\n" +
                "             xmlns:ows=\"http://www.opengis.net/ows/1.1\"\n" +
                "\t\t\t xmlns:cal=\"http://www.brockmann-consult.de/calwps/calwpsL3Parameters-schema.xsd\"\n" +
                "             xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
                "             xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n" +
+               "             xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" +
                "\n" +
                "\t<ows:Identifier>case2-regional~1.5.3~Meris.Case2Regional</ows:Identifier>\n" +
                "\n" +
-               "\t<DataInputs>\n" +
-               "\t\t<Input>\n" +
+               "\t<wps:DataInputs>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>productionType</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<LiteralData>L3</LiteralData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
-               "\t\t<Input>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:LiteralData>L3</wps:LiteralData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>calvalus.beam.bundle</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<LiteralData>beam-4.11.1-SNAPSHOT</LiteralData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
-               "\t\t<Input>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:LiteralData>beam-4.11.1-SNAPSHOT</wps:LiteralData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>doAtmosphericCorrection</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<LiteralData>true</LiteralData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
-               "\t\t<Input>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:LiteralData>true</wps:LiteralData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>outputTosa</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<LiteralData>false</LiteralData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
-               "\t\t<Input>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:LiteralData>false</wps:LiteralData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>outputReflec</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<LiteralData>true</LiteralData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
-               "\t\t<Input>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:LiteralData>true</wps:LiteralData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>outputTransmittance</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<LiteralData>false</LiteralData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
-               "\t\t<Input>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:LiteralData>false</wps:LiteralData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>landExpression</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<LiteralData>toa_reflec_10 &gt; toa_reflec_6 AND toa_reflec_13 &gt; 0.0475</LiteralData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:LiteralData>toa_reflec_10 &gt; toa_reflec_6 AND toa_reflec_13 &gt; 0.0475</wps:LiteralData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
                "\n" +
-               "\t\t<Input>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>calvalus.l3.parameters</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<ComplexData>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:ComplexData>\n" +
                "\t\t\t\t\t<cal:parameters>\n" +
                "\t\t\t\t\t\t<cal:compositingType>MOSAICKING</cal:compositingType>\n" +
                "\t\t\t\t\t\t<cal:planetaryGrid>org.esa.beam.binning.support.PlateCarreeGrid</cal:planetaryGrid>\n" +
@@ -179,18 +181,18 @@ public class ExecuteRequestExtractorTest {
                "\t\t\t\t\t\t\t</cal:aggregator>\n" +
                "\t\t\t\t\t\t</cal:aggregators>\n" +
                "\t\t\t\t\t</cal:parameters>\n" +
-               "\t\t\t\t</ComplexData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
+               "\t\t\t\t</wps:ComplexData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
                "\n" +
-               "\t</DataInputs>\n" +
-               "\t<ResponseForm>\n" +
-               "\t\t<ResponseDocument storeExecuteResponse=\"true\" status=\"true\">\n" +
-               "\t\t\t<Output>\n" +
+               "\t</wps:DataInputs>\n" +
+               "\t<wps:ResponseForm>\n" +
+               "\t\t<wps:ResponseDocument storeExecuteResponse=\"true\" status=\"true\">\n" +
+               "\t\t\t<wps:Output>\n" +
                "\t\t\t\t<ows:Identifier>productionResults</ows:Identifier>\n" +
-               "\t\t\t</Output>\n" +
-               "\t\t</ResponseDocument>\n" +
-               "\t</ResponseForm>\n" +
+               "\t\t\t</wps:Output>\n" +
+               "\t\t</wps:ResponseDocument>\n" +
+               "\t</wps:ResponseForm>\n" +
                "\n" +
                "</wps:Execute>\n";
     }
@@ -207,21 +209,21 @@ public class ExecuteRequestExtractorTest {
                "\n" +
                "\t<ows:Identifier>case2-regional~1.5.3~Meris.Case2Regional</ows:Identifier>\n" +
                "\n" +
-               "\t<DataInputs>\n" +
-               "\t\t<Input>\n" +
+               "\t<wps:DataInputs>\n" +
+               "\t\t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>productionType</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<LiteralData></LiteralData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
-               "\t</DataInputs>\n" +
-               "\t<ResponseForm>\n" +
-               "\t\t<ResponseDocument storeExecuteResponse=\"true\" status=\"true\">\n" +
-               "\t\t\t<Output>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<wps:LiteralData></wps:LiteralData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
+               "\t</wps:DataInputs>\n" +
+               "\t<wps:ResponseForm>\n" +
+               "\t\t<wps:ResponseDocument storeExecuteResponse=\"true\" status=\"true\">\n" +
+               "\t\t\t<wps:Output>\n" +
                "\t\t\t\t<ows:Identifier>productionResults</ows:Identifier>\n" +
-               "\t\t\t</Output>\n" +
-               "\t\t</ResponseDocument>\n" +
-               "\t</ResponseForm>\n" +
+               "\t\t\t</wps:Output>\n" +
+               "\t\t</wps:ResponseDocument>\n" +
+               "\t</wps:ResponseForm>\n" +
                "\n" +
                "</wps:Execute>\n";
     }
@@ -238,26 +240,26 @@ public class ExecuteRequestExtractorTest {
                "\n" +
                "\t<ows:Identifier>urbantep-subsetting~1.0~Subset</ows:Identifier>\n" +
                "\n" +
-               "\t<DataInputs>\n" +
-               "      \t<Input>\n" +
+               "\t<wps:DataInputs>\n" +
+               "      \t<wps:Input>\n" +
                "\t\t\t<ows:Identifier>regionWKT</ows:Identifier>\n" +
-               "\t\t\t<Data>\n" +
-               "\t\t\t\t<BoundingBoxData>\n" +
-               "                  <LowerCorner>100 -10</LowerCorner>\n" +
-               "                  <UpperCorner>110 0</UpperCorner>\n" +
-               "                  <crs>urn:ogc:def:crs:EPSG:6:6:4326</crs>\n" +
-               "                </BoundingBoxData>\n" +
-               "\t\t\t</Data>\n" +
-               "\t\t</Input>\n" +
+               "\t\t\t<wps:Data>\n" +
+               "\t\t\t\t<ows:BoundingBoxData>\n" +
+               "                  <ows:LowerCorner>100 -10</ows:LowerCorner>\n" +
+               "                  <ows:UpperCorner>110 0</ows:UpperCorner>\n" +
+               "                  <ows:crs>urn:ogc:def:crs:EPSG:6:6:4326</ows:crs>\n" +
+               "                </ows:BoundingBoxData>\n" +
+               "\t\t\t</wps:Data>\n" +
+               "\t\t</wps:Input>\n" +
                "\t\t\n" +
-               "\t</DataInputs>\n" +
-               "\t<ResponseForm>\n" +
-               "\t\t<ResponseDocument storeExecuteResponse=\"true\" status=\"true\">\n" +
-               "\t\t\t<Output>\n" +
+               "\t</wps:DataInputs>\n" +
+               "\t<wps:ResponseForm>\n" +
+               "\t\t<wps:ResponseDocument storeExecuteResponse=\"true\" status=\"true\">\n" +
+               "\t\t\t<wps:Output>\n" +
                "\t\t\t\t<ows:Identifier>productionResults</ows:Identifier>\n" +
-               "\t\t\t</Output>\n" +
-               "\t\t</ResponseDocument>\n" +
-               "\t</ResponseForm>\n" +
+               "\t\t\t</wps:Output>\n" +
+               "\t\t</wps:ResponseDocument>\n" +
+               "\t</wps:ResponseForm>\n" +
                "</wps:Execute>";
     }
 
