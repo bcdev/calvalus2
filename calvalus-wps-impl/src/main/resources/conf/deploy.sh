995a0b60d94cc1e0e43ecb24dd841b6c7446183c
Hans Permana <hans.permana@gmail.com>
2016-10-19 09:24:05 +0200

    modify deploy script to symlink to tank staging directory

--- calvalus-wps-impl/src/main/resources/conf/deploy.sh
+++ calvalus-wps-impl/src/main/resources/conf/deploy.sh
@@ -12,6 +12,7 @@ sudo -u $RUN_AS cp log4j.properties $CATALINA_HOME/conf/calvalus
 sudo -u $RUN_AS cp metadata-template.vm $CATALINA_HOME/conf/calvalus
 sudo -u $RUN_AS cp -r local-process-descriptor $CATALINA_HOME/conf/calvalus
 sudo -u $RUN_AS mkdir -p $CATALINA_HOME/webapps/bc-wps/staging
+sudo -u $RUN_AS ln -sf /tank/calvalus/wps/staging $CATALINA_HOME/webapps/bc-wps
 sudo -u $RUN_AS cp staging/directory-listing.xsl $CATALINA_HOME/webapps/bc-wps/staging
 sudo -u $RUN_AS cp staging/directory-listing-readme.txt $CATALINA_HOME/webapps/bc-wps/staging
 sudo -u $RUN_AS cp xsd/calwpsL3Parameters-schema.xsd $CATALINA_HOME/webapps/bc-wps
