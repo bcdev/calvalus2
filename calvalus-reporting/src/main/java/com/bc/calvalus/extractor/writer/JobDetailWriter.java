03af79601f80b6bea3f0317ab4214a91a9131878
Muhammad <muhammshu@yahoo.com>
2017-03-31 11:12:56 +0200

    get the file log name between the  range between

--- calvalus-reporting/src/main/java/com/bc/calvalus/extractor/writer/JobDetailWriter.java
+++ calvalus-reporting/src/main/java/com/bc/calvalus/extractor/writer/JobDetailWriter.java
@@ -2,8 +2,8 @@ package com.bc.calvalus.extractor.writer;
 
 
 import com.bc.calvalus.commons.CalvalusLogger;
-import com.bc.calvalus.extractor.Launcher;
 import com.bc.calvalus.extractor.ExtractCalvalusReportException;
+import com.bc.calvalus.extractor.Launcher;
 import com.bc.calvalus.extractor.configuration.Conf;
 import com.bc.calvalus.extractor.configuration.ConfExtractor;
 import com.bc.calvalus.extractor.counter.CountersType;
@@ -18,36 +18,59 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.text.ParseException;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author muhammad.bc
  */
 public class JobDetailWriter {
+    public static final int DAY_OF_MONTH = 1;
+    public static final int ZERO_HOUR = 0;
     private static final int INTERVAL = 10;
+    private static Logger logger = CalvalusLogger.getLogger();
     private GetEOFJobInfo getEOFJobInfo;
     private File outputFile;
-    private static Logger logger = CalvalusLogger.getLogger();
     private List<JobDetailType> jobDetailTypeList;
+    private String pathToWrite;
 
+
+    public JobDetailWriter() {
+    }
 
     public JobDetailWriter(String pathToWrite) {
         try {
+            this.pathToWrite = pathToWrite;
             jobDetailTypeList = new ArrayList<>();
             outputFile = confirmOutputFile(pathToWrite);
             getEOFJobInfo = new GetEOFJobInfo(outputFile);
         } catch (IOException e) {
             logger.log(Level.SEVERE, e.getMessage());
         }
+    }
+
+    public static void stop() {
+        Thread thread = Thread.currentThread();
+        Launcher.terminate = true;
+        thread.interrupt();
     }
 
     public void start() {
@@ -62,7 +85,99 @@ public class JobDetailWriter {
                     .collect(Collectors.toList());
 
             writeWithIntervals(filterAfterDate);
+
         }
+    }
+
+    Predicate<String> filterDateTimeBtw(String startDateTime, String endDateTime) {
+        return aLong -> {
+
+            Matcher matcher = groupMatchers(aLong);
+            String startDateFrmFileName = matcher.group(1);
+            String endDateFrmFileName = matcher.group(2);
+
+
+            Instant end = LocalDate.parse(endDateFrmFileName).atTime(LocalTime.MAX).toInstant(ZoneOffset.UTC);
+            Instant start = LocalDate.parse(startDateFrmFileName).atStartOfDay().toInstant(ZoneOffset.UTC);
+
+            Instant instantStartDT = null;
+            Instant instantStartET = null;
+            try {
+                instantStartDT = getFirstDayOfMonth(startDateTime).toInstant(ZoneOffset.UTC);
+                instantStartET = getLastDayOfMonth(endDateTime).toInstant(ZoneOffset.UTC);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            boolean startRange = (instantStartDT.isBefore(start) || instantStartDT.equals(start))
+                    && (instantStartET.isAfter(start) || instantStartET.equals(start));
+
+            boolean endRange = (instantStartDT.isBefore(end) || instantStartDT.equals(end))
+                    && (instantStartET.isAfter(end) || instantStartDT.equals(end));
+
+            return startRange || endRange;
+        };
+    }
+
+    Predicate<String> filterDateTime(final String searchDate) {
+        return aLong -> {
+            Matcher matcher = groupMatchers(aLong);
+            String startDateFrmFileName = matcher.group(1);
+            String endDateFrmFileName = matcher.group(2);
+
+
+            Instant end = LocalDate.parse(endDateFrmFileName).atTime(LocalTime.MAX).toInstant(ZoneOffset.UTC);
+            Instant start = LocalDate.parse(startDateFrmFileName).atStartOfDay().toInstant(ZoneOffset.UTC);
+
+            Instant instant = LocalDateTime.parse(searchDate).toInstant(ZoneOffset.UTC);
+            return start.isBefore(instant) && end.isAfter(instant);
+        };
+    }
+
+    File[] getLoggerFiles(String startInterval) {
+        File file = Paths.get(pathToWrite).toFile();
+        String[] listFiles = file.list();
+        Predicate<String> stringPredicate = filterDateTime(startInterval);
+
+        List<String> collect = Stream.of(listFiles)
+                .map(File::new).map(p -> p.getName())
+                .filter(stringPredicate)
+                .collect(Collectors.toList());
+
+        return new File[0];
+    }
+
+    void flushToFile(List<JobDetailType> jobDetailTypeList, File outputFile) {
+        try (
+                FileWriter fileWriter = new FileWriter(outputFile, true);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)
+        ) {
+            Gson gson = new Gson();
+            for (JobDetailType jobDetailType : jobDetailTypeList) {
+                bufferedWriter.append(gson.toJson(jobDetailType));
+                bufferedWriter.write(",");
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+        }
+        logger.log(Level.INFO, String.format("Flush all cache job details to %s", outputFile.toString()));
+    }
+
+    LocalDateTime getLastDayOfMonth(String dateTime) throws ParseException {
+        return LocalDateTime.parse(dateTime).plusMonths(1).withDayOfMonth(DAY_OF_MONTH).minusDays(1).withHour(ZERO_HOUR);
+    }
+
+    LocalDateTime getFirstDayOfMonth(String dateTime) throws ParseException {
+        return LocalDateTime.parse(dateTime).withDayOfMonth(DAY_OF_MONTH).withHour(ZERO_HOUR);
+    }
+
+    @NotNull
+    private Matcher groupMatchers(String aLong) {
+        Pattern compile = Pattern.compile("(\\d{4}-\\d{2}-\\d{2})_To_(\\d{4}-\\d{2}-\\d{2})");
+        Matcher matcher = compile.matcher(aLong);
+        matcher.find();
+        return matcher;
     }
 
     private void writeWithIntervals(List<com.bc.calvalus.extractor.jobs.JobType> sortedJobTypeList) {
@@ -86,7 +201,6 @@ public class JobDetailWriter {
             logger.log(Level.SEVERE, e.getMessage());
         }
     }
-
 
     private void mergeConfCounterRecord(int from, int to, List<com.bc.calvalus.extractor.jobs.JobType> sortedJobTypeList) throws ExtractCalvalusReportException {
         int interval = to - from;
@@ -116,29 +230,6 @@ public class JobDetailWriter {
         jobDetailTypeList.clear();
     }
 
-    void flushToFile(List<JobDetailType> jobDetailTypeList, File outputFile) {
-        try (
-                FileWriter fileWriter = new FileWriter(outputFile, true);
-                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)
-        ) {
-            Gson gson = new Gson();
-            for (JobDetailType jobDetailType : jobDetailTypeList) {
-                bufferedWriter.append(gson.toJson(jobDetailType));
-                bufferedWriter.write(",");
-                bufferedWriter.newLine();
-            }
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, e.getMessage());
-        }
-        logger.log(Level.INFO, String.format("Flush all cache job details to %s", outputFile.toString()));
-    }
-
-    public static void stop() {
-        Thread thread = Thread.currentThread();
-        Launcher.terminate = true;
-        thread.interrupt();
-    }
-
     private File confirmOutputFile(String pathToWrite) throws IOException {
         File file = Paths.get(pathToWrite).toFile();
         if (!file.exists()) {
@@ -155,7 +246,6 @@ public class JobDetailWriter {
         jobDetailTypeList.add(jobDetailType);
     }
 
-
     private HashMap<String, CountersType> createCounterLog(int from, int to, List<com.bc.calvalus.extractor.jobs.JobType> sortedJobTypeList) throws ExtractCalvalusReportException {
         com.bc.calvalus.extractor.counter.CounterExtractor counterLog = new com.bc.calvalus.extractor.counter.CounterExtractor();
         return counterLog.extractInfo(from, to, sortedJobTypeList);
@@ -165,7 +255,6 @@ public class JobDetailWriter {
         ConfExtractor confLog = new ConfExtractor();
         return confLog.extractInfo(from, to, sortedJobTypeList);
     }
-
 
     private List<com.bc.calvalus.extractor.jobs.JobType> sortedJobsType() {
         JobExtractor jobExtractor = new JobExtractor();
@@ -186,6 +275,7 @@ public class JobDetailWriter {
         Collections.sort(listOfJobs, jobTypeComparator);
         return listOfJobs;
     }
+
 
     static class GetEOFJobInfo {
         private JobDetailType lastJobID;
--- calvalus-reporting/src/test/java/com/bc/calvalus/extractor/writer/JobDetailWriterTest.java
+++ calvalus-reporting/src/test/java/com/bc/calvalus/extractor/writer/JobDetailWriterTest.java
@@ -1,19 +1,102 @@
 package com.bc.calvalus.extractor.writer;
 
 import java.io.File;
-import org.junit.Assert;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * @author muhammad.bc.
  */
 public class JobDetailWriterTest {
     private String pathname = getClass().getClassLoader().getResource(".").getFile() + File.separator + "calvalus-reporting.json";
+    private JobDetailWriter jobDetailWriter;
+
+    @Before
+    public void setUp() throws Exception {
+        jobDetailWriter = new JobDetailWriter();
+    }
 
     @Test
     public void readLastJobID() throws Exception {
         File file = new File(pathname);
         JobDetailWriter.GetEOFJobInfo getEOFJobInfo = new JobDetailWriter.GetEOFJobInfo(file);
-        Assert.assertEquals(getEOFJobInfo.getLastJobDetailsType().getJobId(), "job_1484434434570_0376");
+        assertEquals(getEOFJobInfo.getLastJobDetailsType().getJobId(), "job_1484434434570_0376");
+    }
+
+    @Test
+    public void testSingleDateTimePredicate() throws Exception {
+        Predicate<String> stringPredicate = jobDetailWriter.filterDateTime("2016-12-20T00:00");
+        assertTrue(stringPredicate.test("2016-12-01_To_2016-12-31.json"));
+        assertTrue(stringPredicate.test("2016-12-10_To_2016-12-31.json"));
+        assertFalse(stringPredicate.test("2017-12-10_To_2016-12-31.json"));
+    }
+
+    @Test
+    public void testBetweenDateTimePredicate() throws Exception {
+        List<String> fileName = Arrays.asList(
+                "2014-12-01_To_2014-12-31.json",
+                "2015-03-01_To_2015-01-31.json",
+                "2016-11-01_To_2016-11-30.json",
+                "2016-03-01_To_2016-03-31.json",
+                "2017-09-01_To_2017-12-31.json",
+                "2016-12-01_To_2016-12-31.json",
+                "2018-06-01_To_2018-06-30.json",
+                "2019-07-01_To_2019-07-31.json",
+                "2022-12-01_To_2022-12-31.json");
+
+
+        Predicate<String> stringPredicate = jobDetailWriter.filterDateTimeBtw("2016-12-20T00:00", "2016-12-12T00:00");
+        List<String> collectFileBtwn = fileName.stream().filter(stringPredicate).collect(Collectors.toList());
+        assertThat(collectFileBtwn.size(), is(1));
+
+
+
+        Predicate<String> stringPredicateRange = jobDetailWriter.filterDateTimeBtw("2016-12-20T00:00", "2022-12-12T00:00");
+        List<String> collectBtwn = fileName.stream().filter(stringPredicateRange).collect(Collectors.toList());
+        assertThat(collectBtwn.size(), is(5));
+
+    }
+
+    @Test
+    public void firstDayOfMonth() throws Exception {
+        assertThat(jobDetailWriter.getFirstDayOfMonth("2017-04-12T02:00").toString(), is("2017-04-01T00:00"));
+        assertThat(jobDetailWriter.getFirstDayOfMonth("2020-02-29T23:00").toString(), is("2020-02-01T00:00"));
+        assertThat(jobDetailWriter.getFirstDayOfMonth("2020-02-11T23:00").toString(), is("2020-02-01T00:00"));
+    }
+
+    @Test
+    public void lastDayOfMonth() throws Exception {
+        assertThat(jobDetailWriter.getLastDayOfMonth("2014-03-12T02:00").toString(), is("2014-03-31T00:00"));
+        assertThat(jobDetailWriter.getLastDayOfMonth("2019-03-12T02:00").toString(), is("2019-03-31T00:00"));
+        assertThat(jobDetailWriter.getLastDayOfMonth("2020-12-12T02:00").toString(), is("2020-12-31T00:00"));
+        assertThat(jobDetailWriter.getLastDayOfMonth("2021-10-12T02:00").toString(), is("2021-10-31T00:00"));
+    }
+
+    @Test
+    public void testDateNonExist() throws Exception {
+        try {
+            assertThat(jobDetailWriter.getFirstDayOfMonth("2020/02/29T23:00").toString(), is("2020-02-01T00:00"));
+            fail();
+        } catch (DateTimeParseException e) {
+
+        }
+    }
+
+    private JobDetailType createJobDetailType(String finishTime) {
+        JobDetailType jobDetailType = new JobDetailType();
+        jobDetailType.setFinishTime(finishTime);
+        return jobDetailType;
     }
 }
