396c63d58a561bf786b51604a04a24714fd5a407
Muhammad <muhammshu@yahoo.com>
2017-04-19 15:24:45 +0200

    copy log history from remote computer and ignore github

---
+++ calvalus-code-de/src/main/java/com/bc/uban/SSHSession.java
@@ -0,0 +1,24 @@
+package com.bc.uban;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import java.util.Optional;
+
+/**
+ * @author muhammad.bc.
+ */
+public abstract class SSHSession {
+
+    protected Optional<Session> getRemoteSession(String privateKey, String passPhase, String userName, String hostName) throws JSchException {
+        JSch jSch = new JSch();
+        jSch.addIdentity(privateKey, passPhase.getBytes());
+        return Optional.ofNullable(jSch.getSession(userName, hostName));
+    }
+
+    protected Optional<Session> getRemoteSession(String privateKey, String userName, String hostName) throws JSchException {
+        JSch jSch = new JSch();
+        jSch.addIdentity(privateKey);
+        return Optional.ofNullable(jSch.getSession(userName, hostName));
+    }
+}
---
+++ calvalus-code-de/src/main/java/com/bc/uban/UrbanTEPLauncher.java
@@ -0,0 +1,38 @@
+package com.bc.uban;
+
+import com.bc.calvalus.commons.CalvalusLogger;
+import com.bc.wps.utilities.PropertiesWrapper;
+import com.jcraft.jsch.Session;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author muhammad.bc.
+ */
+public class UrbanTEPLauncher {
+    private Logger log = CalvalusLogger.getLogger();
+
+    public UrbanTEPLauncher() {
+        try {
+            PropertiesWrapper.loadConfigFile("urbanTEP.properties");
+            SCPCommand scpCommand = new SCPCommand();
+            Optional<Session> session = scpCommand.getSession();
+            Optional<BufferedReader> bufferedReaderOptional = scpCommand.readFile(session.get());
+            BufferedReader bufferedReader = bufferedReaderOptional.get();
+
+            String s = bufferedReader.readLine();
+            System.out.println("s = " + s);
+
+
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new UrbanTEPLauncher();
+    }
+}
