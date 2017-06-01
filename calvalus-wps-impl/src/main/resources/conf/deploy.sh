cb9ed3b6b9a369b9edb8ca407f5e70720209daee
Hans Permana <hans.permana@gmail.com>
2017-06-01 11:15:07 +0200

    change the location of utep_input for local subsetting

--- calvalus-wps-impl/src/main/resources/conf/deploy.sh
+++ calvalus-wps-impl/src/main/resources/conf/deploy.sh
@@ -13,8 +13,8 @@ sudo -u $RUN_AS cp metadata-template.vm $CATALINA_HOME/conf/calvalus
 sudo -u $RUN_AS cp -r local-process-descriptor $CATALINA_HOME/conf/calvalus
 sudo -u $RUN_AS mkdir -p $CATALINA_HOME/webapps/bc-wps/staging
 sudo -u $RUN_AS ln -sf /tank/calvalus/wps/staging $CATALINA_HOME/webapps/bc-wps
-sudo -u $RUN_AS ln -sf /tank/calvalus/wps/staging/static-content/software
-sudo -u $RUN_AS ln -sf /tank/calvalus/wps/staging/static-content/eodata
+sudo -u $RUN_AS ln -sf /tank/calvalus/wps/staging/static-content/software $CATALINA_HOME/webapps/bc-wps
+sudo -u $RUN_AS ln -sf /tank/calvalus/wps/staging/static-content/eodata $CATALINA_HOME/webapps/bc-wps
 sudo -u $RUN_AS cp staging/directory-listing.xsl $CATALINA_HOME/webapps/bc-wps/staging
 sudo -u $RUN_AS cp staging/directory-listing-readme.txt $CATALINA_HOME/webapps/bc-wps/staging
 sudo -u $RUN_AS cp xsd/calwpsL3Parameters-schema.xsd $CATALINA_HOME/webapps/bc-wps
@@ -24,5 +24,5 @@ sudo -u $RUN_AS cp -r urbantep-quicklooks $CATALINA_HOME/webapps/ROOT
 # don't forget to allow sym link in Tomcat directory.
 # How-to: http://stackoverflow.com/questions/22240776/symlinking-tomcat-8-directory-resources
 sudo -u $RUN_AS ln -sf ../ROOT/urbantep-quicklooks $CATALINA_HOME/webapps/bc-wps
-sudo -u $RUN_AS ln -sf /data/urban-footprint/utep_input $CATALINA_HOME/webapps/bc-wps
+sudo -u $RUN_AS ln -sf /tank/calvalus/wps/utep_input $CATALINA_HOME/webapps/bc-wps
 sudo service bc-tomcat start
