4dd0fe19354e2d797db89cbbf03ea07bbeafc3fa
martin-boettcher <martin.boettcher@brockmann-consult.de>
2017-06-06 13:15:54 +0200

    reconfiguring reporting to avoid that database is removed by tomcat from work dir again

--- calvalus-code-de/src/main/java/com/bc/calvalus/urban/SendWriteMessage.java
+++ calvalus-code-de/src/main/java/com/bc/calvalus/urban/SendWriteMessage.java
@@ -99,7 +99,7 @@ public class SendWriteMessage {
             );
             lastDateTime = parseLocalDateTime(finishDateTime, jobID);
             if (Objects.nonNull(lastDateTime) && !lastCursorPosition.isAfter(lastDateTime)) {
-                logger.log(Level.INFO, String.format("JobID %s processing ..."));
+                logger.log(Level.INFO, String.format("JobID %s processing ...", wpsReport.getJobID()));
                 handleNewWpsReport(wpsReport);
             } else {
                 logger.log(Level.INFO, String.format("JobID %s before %s skipped.", wpsReport.getJobID(), String.valueOf(lastCursorPosition)));
