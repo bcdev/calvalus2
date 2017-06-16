6143e2ac3240ec3cf1e59126323a126a55873505
marpet <marco.peters@brockmann-consult.de>
2017-06-16 12:24:08 +0200

    inlined method

--- calvalus-reporting/src/main/java/com/bc/calvalus/reporting/restservice/ws/PriceCalculator.java
+++ calvalus-reporting/src/main/java/com/bc/calvalus/reporting/restservice/ws/PriceCalculator.java
@@ -14,22 +14,18 @@ class PriceCalculator {
 
     static double getCpuPrice(long totalVCoresUsedSeconds) {
         double totalVCoresUsedHours = totalVCoresUsedSeconds / 3600;
-        return monetize(totalVCoresUsedHours * CPU_PER_THREAD_PER_HOUR_EURO);
+        return totalVCoresUsedHours * CPU_PER_THREAD_PER_HOUR_EURO;
     }
 
     static double getMemoryPrice(long gbMillisMapsReduces) {
         double totalMemoryUsedGbSeconds = (gbMillisMapsReduces) / 1024;
         double totalMemoryUsedGbHours = totalMemoryUsedGbSeconds / 3600;
-        return monetize(totalMemoryUsedGbHours * MEMORY_PER_GB_PER_HOUR_EURO);
+        return totalMemoryUsedGbHours * MEMORY_PER_GB_PER_HOUR_EURO;
     }
 
     static double getDiskPrice(long fileHdfsBytesReadWriteMb) {
         double totalFileReadWriteGb = fileHdfsBytesReadWriteMb / 1024;
-        return monetize(totalFileReadWriteGb * DISK_PER_GB_PER_YEAR_EURO);
+        return totalFileReadWriteGb * DISK_PER_GB_PER_YEAR_EURO;
     }
 
-    private static double monetize(double rawDoubleValue) {
-        long money = (long) (rawDoubleValue * 100);
-        return money / 100.0;
-    }
 }
