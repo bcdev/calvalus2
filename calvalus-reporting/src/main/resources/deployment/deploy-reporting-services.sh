a983b7a17f07b76aba7d1a333d05530949513432
Hans Permana <hans.permana@gmail.com>
2017-07-12 13:46:30 +0200

    update deployment bundle

--- calvalus-reporting/src/main/resources/deployment/deploy-reporting-services.sh
+++ calvalus-reporting/src/main/resources/deployment/deploy-reporting-services.sh
@@ -6,4 +6,6 @@ sudo -u $RUN_AS cp calvalus-reporting.jar $REPORTING_HOME/lib
 sudo -u $RUN_AS cp conf.xsl $REPORTING_HOME/etc
 sudo -u $RUN_AS cp counters.xsl $REPORTING_HOME/etc
 sudo -u $RUN_AS cp collector.sh $REPORTING_HOME
+sudo -u $RUN_AS cp urbantep.sh $REPORTING_HOME
 sudo -u $RUN_AS cp reporting-collector.properties $REPORTING_HOME/etc
+sudo -u $RUN_AS cp urbantep.properties $REPORTING_HOME/etc
