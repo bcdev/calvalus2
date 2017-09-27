a5191fafa3dc35e36c266ed8f5874ee64363c62c
martin-boettcher <martin.boettcher@brockmann-consult.de>
2017-09-27 17:11:22 +0200

    use different relative paths in tests for execution in IDEA and under Maven

--- calvalus-processing/src/test/java/com/bc/calvalus/processing/ra/RARegionsTest.java
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/ra/RARegionsTest.java
@@ -16,14 +16,16 @@
 
 package com.bc.calvalus.processing.ra;
 
-import static org.junit.Assert.*;
-
 import org.apache.hadoop.conf.Configuration;
-import org.junit.*;
+import org.junit.Ignore;
+import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class RARegionsTest {
     
@@ -42,7 +44,13 @@ public class RARegionsTest {
     public void test_iterateOverRegions() throws IOException {
         Configuration conf = new Configuration();
         RAConfig raConfig = new RAConfig();
-        raConfig.setRegionSource("src/test/resources/HELCOM_grid100_LAEA5210.zip");
+        String input;
+        if (new File("src/test/resources/HELCOM_grid100_LAEA5210.zip").exists()) {
+            input = "src/test/resources/HELCOM_grid100_LAEA5210.zip";  // surefire
+        } else {
+            input = "calvalus-processing/src/test/resources/HELCOM_grid100_LAEA5210.zip";  // idea
+        }
+        raConfig.setRegionSource(input);
         raConfig.setRegionSourceAttributeName("CellCode");
         raConfig.setRegionSourceAttributeFilter("100kmE52");
         RARegions.RegionIterator i = raConfig.createNamedRegionIterator(conf);
