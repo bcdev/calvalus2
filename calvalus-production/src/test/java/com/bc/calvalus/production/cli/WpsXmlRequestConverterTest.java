70c14540aa3de97628989605dc6d4475307ff379
Hans Permana <hans.permana@gmail.com>
2018-01-16 15:49:50 +0100

    fix unit tests after the change in this commit https://github.com/bcdev/calvalus2/commit/188d6bdc2495f8decc38b1b079f35641e3d96634

--- calvalus-production/src/test/java/com/bc/calvalus/production/cli/WpsXmlRequestConverterTest.java
+++ calvalus-production/src/test/java/com/bc/calvalus/production/cli/WpsXmlRequestConverterTest.java
@@ -44,7 +44,8 @@ public class WpsXmlRequestConverterTest {
                                        "             xmlns:ows=\"http://www.opengis.net/ows/1.1\"\n" +
                                        "             xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n" +
                                        "             xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-                                       "             xsi:schemaLocation=\"http://www.opengis.net/wps/1.0.0 ogc/wps/1.0.0/wpsExecute_request.xsd\">\n  <ows:Identifier>L2Plus</ows:Identifier>\n" +
+                                       "             xsi:schemaLocation=\"http://www.opengis.net/wps/1.0.0 ogc/wps/1.0.0/wpsExecute_request.xsd\">\n" +
+                                       "  <ows:Identifier>L2Plus</ows:Identifier>\n" +
                                        "  <wps:DataInputs>\n" +
                                        "    <wps:Input>\n" +
                                        "      <ows:Identifier>calvalus.processor.package</ows:Identifier>\n" +
@@ -80,7 +81,8 @@ public class WpsXmlRequestConverterTest {
                                        "      <wps:Data>\n" +
                                        "        <wps:LiteralData>BEAM-DIMAP</wps:LiteralData>\n" +
                                        "      </wps:Data>\n" +
-                                       "    </wps:Input>\n    <wps:Input>\n" +
+                                       "    </wps:Input>\n" +
+                                       "    <wps:Input>\n" +
                                        "      <ows:Identifier>calvalus.l2.operator</ows:Identifier>\n" +
                                        "      <ows:Title/>\n" +
                                        "      <wps:Data>\n" +
@@ -91,19 +93,20 @@ public class WpsXmlRequestConverterTest {
                                        "      <ows:Identifier>calvalus.l2.parameters</ows:Identifier>\n" +
                                        "      <ows:Title/>\n" +
                                        "      <wps:Data>\n" +
-                                       "        <wps:LiteralData><parameters>\n" +
-                                       "   <doSmile>true</doSmile>\n" +
-                                       "    <reproVersion>AUTO_DETECT</reproVersion>\n</parameters></wps:LiteralData>\n" +
+                                       "        <wps:LiteralData>&lt;parameters&gt;\n" +
+                                       "   &lt;doSmile&gt;true&lt;/doSmile&gt;\n" +
+                                       "    &lt;reproVersion&gt;AUTO_DETECT&lt;/reproVersion&gt;\n" +
+                                       "&lt;/parameters&gt;</wps:LiteralData>\n" +
                                        "      </wps:Data>\n" +
                                        "    </wps:Input>\n" +
                                        "    <wps:Input>\n" +
                                        "      <ows:Identifier>calvalus.plainText.parameter</ows:Identifier>\n" +
                                        "      <ows:Title/>\n" +
                                        "      <wps:Data>\n" +
-                                       "        <wps:LiteralData><parameters>\n" +
+                                       "        <wps:LiteralData>&lt;parameters&gt;\n" +
                                        "This is a multiline\n" +
                                        "Textfield\n" +
-                                       "</parameters></wps:LiteralData>\n" +
+                                       "&lt;/parameters&gt;</wps:LiteralData>\n" +
                                        "      </wps:Data>\n" +
                                        "    </wps:Input>\n" +
                                        "  </wps:DataInputs>\n" +
@@ -115,19 +118,20 @@ public class WpsXmlRequestConverterTest {
         for (String key : keySet) {
             String orig = productionParameters.get(key);
             String now = requestRestored.getString(key);
-            assertEquals("key="+key, orig, now);
+            assertEquals("key=" + key, orig, now);
         }
     }
 
     @Test
-    public void toXmlMultipleInputPath() throws Exception {
+    public void toXmlMultipleInputPath() {
         Map<String, String> productionParameters = new HashMap<>();
         productionParameters.put("calvalus.processor.package", "beam-meris-radiometry");
         productionParameters.put("calvalus.processor.version", "1.0-SNAPSHOT");
         productionParameters.put("calvalus.input.format", "BEAM-DIMAP");
         productionParameters.put("calvalus.output.dir", "hdfs://master00:9000/calvalus/outputs/meris-l2beam-99");
-        productionParameters.put("calvalus.input", "hdfs://master00:9000/calvalus/eodata/MER_RR__1P/r03/2004/07/15/MER_RR__1PRACR20040715_011806_000026382028_00332_12410_0000.N1," +
-                                                   "hdfs://master00:9000/calvalus/eodata/MER_RR__1P/r03/2004/07/15/MER_RR__1PRACR20040715_021806_000026382028_00332_12410_0000.N1");
+        productionParameters.put("calvalus.input",
+                                 "hdfs://master00:9000/calvalus/eodata/MER_RR__1P/r03/2004/07/15/MER_RR__1PRACR20040715_011806_000026382028_00332_12410_0000.N1," +
+                                 "hdfs://master00:9000/calvalus/eodata/MER_RR__1P/r03/2004/07/15/MER_RR__1PRACR20040715_021806_000026382028_00332_12410_0000.N1");
         productionParameters.put("calvalus.l2.operator", "Meris.CorrectRadiometry");
         productionParameters.put("calvalus.l2.parameters", "<parameters>\n" +
                                                            "   <doSmile>true</doSmile>\n" +
@@ -197,19 +201,19 @@ public class WpsXmlRequestConverterTest {
                                        "      <ows:Identifier>calvalus.l2.parameters</ows:Identifier>\n" +
                                        "      <ows:Title/>\n" +
                                        "      <wps:Data>\n" +
-                                       "        <wps:LiteralData><parameters>\n" +
-                                       "   <doSmile>true</doSmile>\n" +
-                                       "    <reproVersion>AUTO_DETECT</reproVersion>\n</parameters></wps:LiteralData>\n" +
+                                       "        <wps:LiteralData>&lt;parameters&gt;\n" +
+                                       "   &lt;doSmile&gt;true&lt;/doSmile&gt;\n" +
+                                       "    &lt;reproVersion&gt;AUTO_DETECT&lt;/reproVersion&gt;\n&lt;/parameters&gt;</wps:LiteralData>\n" +
                                        "      </wps:Data>\n" +
                                        "    </wps:Input>\n" +
                                        "    <wps:Input>\n" +
                                        "      <ows:Identifier>calvalus.plainText.parameter</ows:Identifier>\n" +
                                        "      <ows:Title/>\n" +
                                        "      <wps:Data>\n" +
-                                       "        <wps:LiteralData><parameters>\n" +
+                                       "        <wps:LiteralData>&lt;parameters&gt;\n" +
                                        "This is a multiline\n" +
                                        "Textfield\n" +
-                                       "</parameters></wps:LiteralData>\n" +
+                                       "&lt;/parameters&gt;</wps:LiteralData>\n" +
                                        "      </wps:Data>\n" +
                                        "    </wps:Input>\n" +
                                        "  </wps:DataInputs>\n" +
@@ -217,7 +221,7 @@ public class WpsXmlRequestConverterTest {
     }
 
     @Test
-    public void toXmlWhenNoDataInputs() throws Exception {
+    public void toXmlWhenNoDataInputs() {
         Map<String, String> productionParameters = new HashMap<>();
         ProductionRequest productionRequest = new ProductionRequest("L2Plus", "userName", productionParameters);
 
