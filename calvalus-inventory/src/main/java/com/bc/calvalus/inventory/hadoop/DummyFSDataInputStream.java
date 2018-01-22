ee382253c35f8fb2e1fb58d169927e7a910639fe
Hans Permana <hans.permana@gmail.com>
2018-01-22 15:53:44 +0100

    fix logging not accessible issue in CODE Calvalus Portal
    (cherry picked from commit 548519b)

--- calvalus-inventory/src/main/java/com/bc/calvalus/inventory/hadoop/DummyFSDataInputStream.java
+++ calvalus-inventory/src/main/java/com/bc/calvalus/inventory/hadoop/DummyFSDataInputStream.java
@@ -1,5 +1,6 @@
 package com.bc.calvalus.inventory.hadoop;
 
+import com.bc.calvalus.commons.CalvalusLogger;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.PositionedReadable;
 import org.apache.hadoop.fs.ReadOption;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.EnumSet;
+import java.util.logging.Level;
 
 /**
  * Dummy implementation of a formally seekable stream to obey the Hadoop FileSystem interface.
@@ -21,6 +23,9 @@ import java.util.EnumSet;
  */
 public class DummyFSDataInputStream extends FSDataInputStream {
 
+    private String path;
+    private CalvalusShFileSystem fileSystem;
+
     static class DummyByteArrayInputStream extends ByteArrayInputStream implements Seekable, PositionedReadable {
 
         public DummyByteArrayInputStream(byte[] buf) {
@@ -29,7 +34,7 @@ public class DummyFSDataInputStream extends FSDataInputStream {
 
         @Override
         public void seek(long pos) throws IOException {
-             throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException();
         }
 
         @Override
@@ -58,41 +63,135 @@ public class DummyFSDataInputStream extends FSDataInputStream {
         }
     }
 
-    public DummyFSDataInputStream(InputStream in) {
+    static class DummyCountingInputStream extends InputStream {
+
+        private InputStream in;
+        private long pos = 0;
+
+        public DummyCountingInputStream(InputStream in) {
+            super();
+            this.in = in;
+        }
+
+        public void setIn(InputStream in) {
+            this.in = in;
+            pos = 0;
+        }
+
+        @Override
+        public int read() throws IOException {
+            ++pos;
+            return in.read();
+        }
+
+        @Override
+        public int read(byte[] b) throws IOException {
+            int count = in.read(b);
+            pos += count;
+            return count;
+        }
+
+        @Override
+        public int read(byte[] b, int off, int len) throws IOException {
+            int count = in.read(b, off, len);
+            pos += count;
+            return count;
+        }
+
+        @Override
+        public long skip(long n) throws IOException {
+            return in.skip(n);
+        }
+
+        @Override
+        public int available() throws IOException {
+            return in.available();
+        }
+
+        @Override
+        public void close() throws IOException {
+            in.close();
+        }
+
+        @Override
+        public synchronized void mark(int readlimit) {
+            in.mark(readlimit);
+        }
+
+        @Override
+        public synchronized void reset() throws IOException {
+            in.reset();
+        }
+
+        @Override
+        public boolean markSupported() {
+            return in.markSupported();
+        }
+
+        public long getPos() {
+            return pos;
+        }
+    }
+
+    public DummyFSDataInputStream(InputStream in, String path, CalvalusShFileSystem fileSystem) {
         super(new DummyByteArrayInputStream(new byte[2]));
-        this.in = in;
+        this.in = new DummyCountingInputStream(in);
+        this.path = path;
+        this.fileSystem = fileSystem;
     }
 
     /**
-     *Verbatim copy from: java.io.InputStream#skip(long) 
+     * Verbatim copy from: java.io.InputStream#skip(long)
      * to prevent FileInputstream from skipping wrongly
      */
     @Override
     public long skip(long n) throws IOException {
+        CalvalusLogger.getLogger().log(Level.INFO, "Start skip " + n +
+                                                   " with current position " +
+                                                   ((DummyCountingInputStream) this.in).getPos());
 
         long remaining = n;
         int nr;
 
         if (n <= 0) {
+            CalvalusLogger.getLogger().log(Level.INFO, "n = " + n);
             return 0;
         }
 
-        int size = (int)Math.min(8192, remaining);
+        int size = (int) Math.min(8192, remaining);
         byte[] skipBuffer = new byte[size];
         while (remaining > 0) {
-            nr = read(skipBuffer, 0, (int)Math.min(size, remaining));
+            CalvalusLogger.getLogger().log(Level.INFO, "Math.min(size, remaining) : " + Math.min(size, remaining));
+            CalvalusLogger.getLogger().log(Level.INFO, "remaining : " + remaining);
+            nr = read(skipBuffer, 0, (int) Math.min(size, remaining));
+            CalvalusLogger.getLogger().log(Level.INFO, "nr : " + nr);
             if (nr < 0) {
                 break;
             }
             remaining -= nr;
+            CalvalusLogger.getLogger().log(Level.INFO, "remaining stream : " + remaining);
         }
-
         return n - remaining;
     }
 
     @Override
     public void seek(long desired) throws IOException {
-        throw new UnsupportedOperationException();
+        CalvalusLogger.getLogger().log(Level.INFO, "Start seek " + desired + " with current position " +
+                                                   ((DummyCountingInputStream) this.in).getPos());
+        DummyCountingInputStream in = (DummyCountingInputStream) this.in;
+        long pos = in.getPos();
+        if (desired >= pos) {
+            long skip = skip(desired - pos);
+        } else {
+            InputStream inputStream = fileSystem.callUnixCommand("cat", path).getInputStream();
+            in.setIn(inputStream);
+            long skip = skip(desired);
+        }
+    }
+
+    @Override
+    public int read() throws IOException {
+        return super.read();
     }
 
     @Override
@@ -146,7 +245,8 @@ public class DummyFSDataInputStream extends FSDataInputStream {
     }
 
     @Override
-    public ByteBuffer read(ByteBufferPool bufferPool, int maxLength, EnumSet<ReadOption> opts) throws IOException, UnsupportedOperationException {
+    public ByteBuffer read(ByteBufferPool bufferPool, int maxLength, EnumSet<ReadOption> opts)
+                throws IOException, UnsupportedOperationException {
         throw new UnsupportedOperationException();
     }
 
