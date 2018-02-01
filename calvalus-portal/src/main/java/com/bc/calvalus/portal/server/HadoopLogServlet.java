d746192c96f66993576ad476ed31033e1445386a
Hans Permana <hans.permana@gmail.com>
2018-02-01 10:16:13 +0100

    refactor log handling in HadoopLogServlet.java  to a new class ProcessingLogHandler so that it can be used by cpt
    (cherry picked from commit 579a408)

--- calvalus-portal/src/main/java/com/bc/calvalus/portal/server/HadoopLogServlet.java
+++ calvalus-portal/src/main/java/com/bc/calvalus/portal/server/HadoopLogServlet.java
@@ -18,60 +18,26 @@ package com.bc.calvalus.portal.server;
 
 import static com.bc.calvalus.portal.server.BackendServiceImpl.getUserName;
 
-import com.bc.calvalus.commons.CalvalusLogger;
 import com.bc.calvalus.commons.ProcessState;
 import com.bc.calvalus.commons.WorkflowItem;
-import com.bc.calvalus.processing.hadoop.HadoopProcessingService;
-import com.bc.calvalus.processing.hadoop.HadoopWorkflowItem;
 import com.bc.calvalus.production.Production;
 import com.bc.calvalus.production.ProductionException;
 import com.bc.calvalus.production.ServiceContainer;
-import com.bc.calvalus.production.store.ProxyWorkflow;
-import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileContext;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.RemoteIterator;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.RunningJob;
-import org.apache.hadoop.mapred.TaskAttemptID;
-import org.apache.hadoop.mapred.TaskCompletionEvent;
-import org.apache.hadoop.mapreduce.JobID;
-import org.apache.hadoop.security.UserGroupInformation;
-import org.apache.hadoop.yarn.api.records.ApplicationId;
-import org.apache.hadoop.yarn.conf.YarnConfiguration;
-import org.apache.hadoop.yarn.logaggregation.AggregatedLogFormat;
-import org.apache.hadoop.yarn.logaggregation.LogAggregationUtils;
-import org.apache.hadoop.yarn.util.ConverterUtils;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.EOFException;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.security.PrivilegedExceptionAction;
-import java.util.logging.Level;
 
 /**
  * Servlet to handle log file viewing for productions
  *
  * @author MarcoZ
+ * @author Hans
  */
 public class HadoopLogServlet extends HttpServlet {
-
-    private static final int KILO_BYTES = 1024;
-
-    private BackendConfig backendConfig;
-    private boolean withExternalAccessControl;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
@@ -80,251 +46,32 @@ public class HadoopLogServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        ServletOutputStream outputStream = resp.getOutputStream();
         String productionId = req.getParameter("productionId");
-        if (productionId == null) {
-            showErrorPage("Missing query parameter 'productionId'", resp);
-            return;
-        }
         ServiceContainer serviceContainer = (ServiceContainer) getServletContext().getAttribute("serviceContainer");
-        backendConfig = new BackendConfig(getServletContext());
-        withExternalAccessControl = serviceContainer.getHadoopConfiguration().getBoolean(
+        BackendConfig backendConfig = new BackendConfig(getServletContext());
+        boolean withExternalAccessControl = serviceContainer.getHadoopConfiguration().getBoolean(
                     "calvalus.accesscontrol.external", false);
-        try {
-            Production production = serviceContainer.getProductionService().getProduction(productionId);
-            final String userName = getUserName(req).toLowerCase();
-            ProcessState processState = production.getProcessingStatus().getState();
-            WorkflowItem workflow = production.getWorkflow();
-            CalvalusLogger.getLogger().log(Level.INFO, "processState = ", processState.toString());
-            if (processState == ProcessState.ERROR) {
-                handleWorkFlow(workflow, ProcessState.ERROR, resp, TaskCompletionEvent.Status.FAILED, userName);
-            } else if (processState == ProcessState.COMPLETED) {
-                handleWorkFlow(workflow, ProcessState.COMPLETED, resp, TaskCompletionEvent.Status.SUCCEEDED, userName);
-            } else {
-                showErrorPage("Logfile are only supported for ERROR or COMPLETED states", resp);
-            }
-        } catch (ProductionException e) {
-            showErrorPage("Failed to get production for id: " + productionId, resp);
-        }
-    }
-
-    private void handleWorkFlow(WorkflowItem workflow, ProcessState processState, HttpServletResponse resp,
-                                TaskCompletionEvent.Status status, String userName) throws IOException {
-        WorkflowItem[] items = workflow.getItems();
-        if (items.length == 0) {
-            // the one and only item must be the failed one
-            showLogFor(workflow, resp, status, userName);
+        ProcessingLogHandler logHandler = new ProcessingLogHandler(backendConfig, withExternalAccessControl);
+        int responseStatus;
+        if (productionId == null) {
+            responseStatus = logHandler.showErrorPage("Missing query parameter 'productionId'", outputStream);
         } else {
-            for (WorkflowItem workflowItem : items) {
-                if (workflowItem.getStatus().getState() == processState) {
-                    handleWorkFlow(workflowItem, processState, resp, status, userName);
-                    return;
-                }
-            }
-        }
-    }
-
-    private void showLogFor(WorkflowItem workflowItem, HttpServletResponse resp, TaskCompletionEvent.Status status,
-                            String userName) throws IOException {
-        CalvalusLogger.getLogger().log(Level.INFO, "showing log with status " + status);
-        if (workflowItem instanceof HadoopWorkflowItem) {
-            HadoopWorkflowItem hadoopWorkflowItem = (HadoopWorkflowItem) workflowItem;
-
-            HadoopProcessingService processingService = hadoopWorkflowItem.getProcessingService();
-            JobClient jobClient = processingService.getJobClient(userName);
-            showLogFor(resp, workflowItem, jobClient, status, userName);
-        } else if (workflowItem instanceof ProxyWorkflow) {
-            ProxyWorkflow proxyWorkflow = (ProxyWorkflow) workflowItem;
-
-            HadoopProcessingService processingService = (HadoopProcessingService) proxyWorkflow.getProcessingService();
-            JobClient jobClient = processingService.getJobClient(userName);
-            showLogFor(resp, workflowItem, jobClient, status, userName);
-        } else {
-            showErrorPage("Not able to provide logs.", resp);
-        }
-    }
-
-    private void showLogFor(HttpServletResponse resp, WorkflowItem hadoopWorkflowItem, JobClient jobClient,
-                            TaskCompletionEvent.Status status, String userName) throws IOException {
-        Object[] jobIds = hadoopWorkflowItem.getJobIds();
-        if (jobIds.length != 1) {
-            showErrorPage("found not one jobId, but:" + jobIds.length, resp);
-            return;
-        }
-        JobID jobId = (JobID) jobIds[0];
-        org.apache.hadoop.mapred.JobID downgradeJobId = org.apache.hadoop.mapred.JobID.downgrade(jobId);
-        try {
-            RunningJob runningJob = jobClient.getJob(downgradeJobId);
-            Configuration conf = jobClient.getConf();
-            CalvalusLogger.getLogger().log(Level.INFO, "runningJob = " + runningJob);
-            if (runningJob == null) {
-                showErrorPage("No 'RunningJob' found for jobId: " + jobId, resp);
-                return;
-            }
-
-            UserGroupInformation remoteUser;
-            if (withExternalAccessControl) {
-                remoteUser = UserGroupInformation.createRemoteUser("yarn");
-            } else {
-                remoteUser = UserGroupInformation.createRemoteUser(userName);
-            }
             try {
-                remoteUser.doAs(new PrivilegedExceptionAction<Integer>() {
-                    @Override
-                    public Integer run() throws Exception {
-                        StringBuilder appSB = new StringBuilder("application");
-                        String appIdStr = jobId.appendTo(appSB).toString();
-
-                        ApplicationId appId = null;
-                        try {
-                            appId = ConverterUtils.toApplicationId(appIdStr);
-                        } catch (Exception e) {
-                            System.err.println("Invalid ApplicationId specified: " + appIdStr);
-                            return -1;
-                        }
-                        int resultCode = dumpAllContainersLogs(appId, userName, resp.getOutputStream(), conf);
-                        if (resultCode != 0) {
-                            showErrorPage("Failed to open Logfile.", resp);
-                            return -1;
-                        }
-                        return 0;
-                    }
-                });
-            } catch (InterruptedException e) {
-                throw new IOException(e);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            showErrorPage("I/O error", resp);
-        }
-    }
-
-    private String getTaskLogURL(TaskAttemptID taskId, String baseUrl) {
-        return (baseUrl + "/tasklog?attemptid=" + taskId);
-    }
-
-    private void displayTaskLogs(TaskAttemptID taskId, String baseUrl, HttpServletResponse resp) throws IOException {
-        // The tasktracker for a 'failed/killed' job might not be around...
-        if (baseUrl != null) {
-            // Construct the url for the tasklogs
-            String taskLogUrl = getTaskLogURL(taskId, baseUrl);
-
-            URL url = new URL(taskLogUrl);
-            InputStream inputStream = null;
-            try {
-                inputStream = url.openStream();
-                copyIntoOutput(inputStream, resp.getOutputStream());
-            } catch (IOException e) {
-                showErrorPage("Failed to open Logfile." + e.getMessage(), resp);
-            } finally {
-                if (inputStream != null) {
-                    inputStream.close();
+                Production production = serviceContainer.getProductionService().getProduction(productionId);
+                if (production == null) {
+                    throw new ProductionException("Failed to get production for id: " + productionId);
                 }
-            }
-        } else {
-            showErrorPage("No 'baseUrl' given.", resp);
-        }
-    }
-
-    private void copyIntoOutput(InputStream input, OutputStream outputStream) throws IOException {
-        try (BufferedOutputStream output = new BufferedOutputStream(outputStream, 4 * 1024 * 1024)) {
-            byte[] buffer = new byte[1024 * 1024];
-            int bytesRead, bytesWritten = 0;
-            while ((bytesRead = input.read(buffer)) != -1) {
-                output.write(buffer, 0, bytesRead);
-                bytesWritten += bytesRead;
-            }
-            output.flush();
-            log(bytesWritten + " bytes sent");
-        }
-    }
-
-    private void showErrorPage(String message, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter writer = new PrintWriter(response.getOutputStream());
-        writer.println("<html>");
-        writer.println("<title>Error while showing processing log</title>");
-        writer.println("<body>");
-        writer.println("<h1>Error while showing processing log</h1>");
-        writer.println("While retrieving the logfile from the Hadoop Cluster an error occurred.</br>" +
-                       "In most cases this is related to the fact, that the logs are automatically removed after roughly 24 hours.</br>" +
-                       "Depending on the cluster activity this can happen sooner or later.</br></br></br>");
-        writer.println("<hr>");
-        writer.println("<h5>Internal error message</h5>");
-        writer.println(message);
-        writer.println("</body>");
-        writer.flush();
-        writer.close();
-    }
-
-
-    private int dumpAllContainersLogs(ApplicationId appId, String appOwner,
-                                      OutputStream outputStream, Configuration conf) throws IOException {
-        Path remoteRootLogDir = new Path(conf.get(
-                    YarnConfiguration.NM_REMOTE_APP_LOG_DIR,
-                    YarnConfiguration.DEFAULT_NM_REMOTE_APP_LOG_DIR));
-        String logDirSuffix = LogAggregationUtils.getRemoteNodeLogDirSuffix(conf);
-        // TODO Change this to get a list of files from the LAS.
-        Path remoteAppLogDir = LogAggregationUtils.getRemoteAppLogDir(
-                    remoteRootLogDir, appId, appOwner, logDirSuffix);
-        RemoteIterator<FileStatus> nodeFiles;
-        try {
-            nodeFiles = FileContext.getFileContext(conf).listStatus(remoteAppLogDir);
-        } catch (FileNotFoundException fnf) {
-            System.out.println("Logs not available at " + remoteAppLogDir.toString());
-            System.out
-                        .println("Log aggregation has not completed or is not enabled.");
-            return -1;
-        }
-        try (CountablePrintStream countablePrintStream = new CountablePrintStream(outputStream)) {
-            while (nodeFiles.hasNext()) {
-                FileStatus thisNodeFile = nodeFiles.next();
-                AggregatedLogFormat.LogReader reader = new AggregatedLogFormat.LogReader(
-                            conf, new Path(remoteAppLogDir, thisNodeFile.getPath().getName()));
-                try {
-                    DataInputStream valueStream;
-                    AggregatedLogFormat.LogKey key = new AggregatedLogFormat.LogKey();
-                    valueStream = reader.next(key);
-
-                    while (valueStream != null) {
-                        String containerString = "\n\nContainer: " + key + " on " + thisNodeFile.getPath().getName();
-                        countablePrintStream.println(containerString);
-                        countablePrintStream.println(StringUtils.repeat("=", containerString.length()));
-                        long logMaxSizeBytes;
-                        try {
-                            logMaxSizeBytes = Long.parseLong(
-                                        backendConfig.getConfigMap().get("log.max.size.kb")) * KILO_BYTES;
-                        } catch (NumberFormatException exception) {
-                            logMaxSizeBytes = 0;
-                        }
-                        if (logMaxSizeBytes == 0 || countablePrintStream.getCount() < logMaxSizeBytes) {
-                            while (true) {
-                                try {
-                                    AggregatedLogFormat.LogReader.readAContainerLogsForALogType(valueStream,
-                                                                                                countablePrintStream);
-                                } catch (EOFException eof) {
-                                    CalvalusLogger.getLogger().log(Level.INFO, "Finished reading a file. " +
-                                                                               "Accumulated size: " + countablePrintStream.getCount());
-                                    break;
-                                }
-                            }
-                        } else {
-                            String message = String.format(
-                                        "Log file contents have been omitted because it has already " +
-                                        "reached the maximum limit of %d Bytes.", logMaxSizeBytes);
-                            countablePrintStream.println(StringUtils.repeat("-", message.length()));
-                            countablePrintStream.println(message);
-                            countablePrintStream.println(StringUtils.repeat("-", message.length()));
-                        }
-
-                        // Next container
-                        key = new AggregatedLogFormat.LogKey();
-                        valueStream = reader.next(key);
-                    }
-                } finally {
-                    reader.close();
-                }
+                final String userName = getUserName(req).toLowerCase();
+                WorkflowItem workflow = production.getWorkflow();
+                responseStatus = logHandler.handleWorkFlow(workflow, ProcessState.ERROR, outputStream, userName);
+            } catch (ProductionException e) {
+                responseStatus = logHandler.showErrorPage("Failed to get production for id: " + productionId,
+                                                          outputStream);
             }
         }
-        return 0;
+        if (responseStatus != 0) {
+            resp.setContentType("text/html");
+        }
     }
 }
