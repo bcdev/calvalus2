dc3ee3ab00ba315302cc4e6c1c2f858e1c0c6875
hans-permana <hans.permana@gmail.com>
2018-02-14 11:44:12 +0100

    include bulk option to send bulk jms messages as a mean of stress testing

--- calvalus-reporting/src/main/java/com/bc/calvalus/reporting/code/JmsSender.java
+++ calvalus-reporting/src/main/java/com/bc/calvalus/reporting/code/JmsSender.java
@@ -1,5 +1,7 @@
 package com.bc.calvalus.reporting.code;
 
+import com.bc.wps.utilities.PropertiesWrapper;
+
 import javax.jms.JMSException;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -12,33 +14,54 @@ import java.nio.file.Paths;
  */
 public class JmsSender {
 
+    private static final int MAX_SEND_COUNT = 10;
+
     public static void main(String[] args) {
         if (args.length < 3) {
             System.out.println("Invalid arguments.");
             System.exit(-1);
         }
+        System.out.println(args.length);
+        for (String arg : args) {
+            System.out.println("arg: " + arg);
+        }
         String messageConsumerUrl = args[0];
         String queueName = args[1];
         String accountingFilePath = args[2];
+        boolean bulkSend = false;
+        if (args.length > 3 && "bulk".equalsIgnoreCase(args[3])) {
+            bulkSend = true;
+        }
         JmsClient jmsClient = null;
         try {
+            PropertiesWrapper.loadConfigFile("code.properties");
+            int maxSendCount = MAX_SEND_COUNT;
+            try {
+                int count = PropertiesWrapper.getInteger("jms.bulk.count");
+                if (count > 0) {
+                    maxSendCount = count;
+                }
+            } catch (NumberFormatException ignored) {
+            }
             jmsClient = new JmsClient(messageConsumerUrl, queueName);
             Path filePath = Paths.get(accountingFilePath);
             String jsonContent = new String(Files.readAllBytes(filePath));
             System.out.println("messageConsumerUrl: " + messageConsumerUrl);
             System.out.println("queueName: " + queueName);
             System.out.println("accountingFilePath: " + accountingFilePath);
-            jmsClient.sendMessage(jsonContent);
-            System.out.println("========content-sent========");
-            System.out.println(jsonContent);
-            System.out.println("========content-sent========");
+            int sendCount = 0;
+            do {
+                jmsClient.sendMessage(jsonContent);
+                sendCount++;
+                System.out.println("Sent message " + sendCount);
+            } while (bulkSend && sendCount < maxSendCount);
             System.exit(0);
         } catch (JMSException | URISyntaxException | IOException exception) {
             exception.printStackTrace();
             System.exit(-1);
         } finally {
             try {
-                if(jmsClient != null){
+                if (jmsClient != null) {
                     jmsClient.closeConnection();
                 }
             } catch (JMSException ignored) {
