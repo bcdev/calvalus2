ac99a7d83e19f7b1bc4b70d7b08a512cf23aba3f
martin-boettcher <martin.boettcher@brockmann-consult.de>
2018-02-15 17:45:05 +0100

    GranuleTilesInputFormat for S2 time stack processing, selects one split per granule ID

---
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/hadoop/GranuleTilesInputFormat.java
@@ -0,0 +1,89 @@
+package com.bc.calvalus.processing.hadoop;
+
+import com.bc.calvalus.commons.InputPathResolver;
+import com.bc.calvalus.processing.productinventory.ProductInventory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * An input format that maps each stack of weekly tiles to a single (file) split containing only the first tile.
+ * <p/>
+ * Input files are given by the configuration parameter
+ * {@link com.bc.calvalus.processing.JobConfigNames#CALVALUS_INPUT_PATH_PATTERNS CALVALUS_INPUT_PATH_PATTERNS}.
+ * Its value is expected to be a comma-separated list of file path patterns (HDFS URLs).
+ * These patterns can contain dates and region names.
+ *
+ * @author Martin
+ */
+public class GranuleTilesInputFormat extends PatternBasedInputFormat {
+
+    // ESACCI-LC-L3-SR-MERIS-300m-P7D-h36v08-20090108-v2.0.nc
+    static final Pattern FILENAME_PATTERN =
+            Pattern.compile("S2._(?:OPER_PRD_MSIL1C_PDMC_........T......_R..._V........T......_........T......|" +
+                                    "MSIL1C_........T......_N...._R...)" +
+                                    "_(T.....)" +
+                                    "(?:|_........T......)" +
+                                    ".zip");
+    @Override
+    protected void createSplits(ProductInventory productInventory,
+                                RemoteIterator<LocatedFileStatus> fileStatusIt,
+                                List<InputSplit> splits,
+                                Configuration conf, int requestSizeLimit) throws IOException {
+        while (fileStatusIt.hasNext()) {
+            LocatedFileStatus locatedFileStatus = fileStatusIt.next();
+            String fileName = locatedFileStatus.getPath().getName();
+            if (tileNameIn(tileNameOf(fileName), splits)) {
+                continue;
+            }
+            LOG.info("tile " + fileName + " represents " + tileNameOf(fileName));
+            InputSplit split = createSplit(productInventory, conf, locatedFileStatus);
+            if (split != null) {
+                splits.add(split);
+                if (requestSizeLimit > 0 && splits.size() == requestSizeLimit) {
+                    break;
+                }
+            }
+        }
+    }
+
+    protected FileSystem getFileSystem(FileStatus file, Configuration conf) throws IOException {
+        return file.getPath().getFileSystem(conf);
+    }
+
+    private static boolean tileNameIn(String tileName, List<InputSplit> splits) {
+        for (InputSplit split : splits) {
+            if (tileName.equals(tileNameOf(((FileSplit)split).getPath().getName()))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String tileNameOf(String fileName) {
+        Matcher matcher = FILENAME_PATTERN.matcher(fileName);
+        if (! matcher.matches()) {
+            throw new IllegalArgumentException("file name does not match pattern " + FILENAME_PATTERN.pattern() + ": " + fileName);
+        }
+        return matcher.group(1);
+    }
+
+    // multi-year variant
+    protected List<String> getInputPatterns(String inputPathPatterns, Date minDate, Date maxDate, String regionName) {
+        InputPathResolver inputPathResolver = new InputPathResolver();
+        inputPathResolver.setMinDate(minDate);
+        inputPathResolver.setMaxDate(maxDate);
+        inputPathResolver.setRegionName(regionName);
+        return inputPathResolver.resolveMultiYear(inputPathPatterns);
+    }
+}
---
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/hadoop/GranuleTilesInputFormatTest.java
@@ -0,0 +1,55 @@
+package com.bc.calvalus.processing.hadoop;
+
+import com.bc.calvalus.processing.l3.seasonal.SeasonalTilesInputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.BlockLocation;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * TODO add API doc
+ *
+ * @author Martin Boettcher
+ */
+public class GranuleTilesInputFormatTest {
+    @Test
+    public void testCreateSplits() throws Exception {
+        BlockLocation[] blockLocations = {new BlockLocation(new String[]{"name"}, new String[]{"host"}, 0, 99)};
+        GranuleTilesInputFormat inputFormat = new GranuleTilesInputFormat();
+        FileStatus[] files = new FileStatus[] {
+                new FileStatus(99, false, 1, 99, 0, new Path("/ab/cd/S2A_MSIL1C_20170224T180431_N0204_R041_T12RWV_20170224T180427.zip")),
+                new FileStatus(99, false, 1, 99, 0, new Path("/ab/cd/S2A_MSIL1C_20170214T180431_N0204_R041_T12RWV_20170214T180427.zip")),
+                new FileStatus(99, false, 1, 99, 0, new Path("/ab/cd/S2A_MSIL1C_20170215T180431_N0204_R041_T12XXX_20170215T180427.zip")),
+                new FileStatus(99, false, 1, 99, 0, new Path("/ab/cd/S2B_MSIL1C_20170217T180431_N0204_R041_T12RWV_20170217T180427.zip")),
+                new FileStatus(99, false, 1, 99, 0, new Path("/ab/cd/S2A_OPER_PRD_MSIL1C_PDMC_20161123T211306_R141_V20161123T175652_20161123T175652_T12RWV.zip")),
+        };
+        List<InputSplit> splits = new ArrayList<>();
+        Iterator<FileStatus> iterator = Arrays.asList(files).iterator();
+        RemoteIterator<LocatedFileStatus> remoteIter = new RemoteIterator<LocatedFileStatus>() {
+            @Override
+            public boolean hasNext() throws IOException {
+                return iterator.hasNext();
+            }
+
+            @Override
+            public LocatedFileStatus next() throws IOException {
+                return new LocatedFileStatus(iterator.next(), blockLocations);
+            }
+        };
+        inputFormat.createSplits(null, remoteIter, splits, new Configuration(), 0);
+
+        assertEquals(2, splits.size());
+    }
+}
