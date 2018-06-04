2909eb8d723d568ae6c5b6a43f6365e1416f746b
martin-boettcher <martin.boettcher@brockmann-consult.de>
2018-06-04 06:16:17 +0200

    Calvalus Hadoop Tool tested until job submission, still all default parameters

--- calvalus-production/src/main/java/com/bc/calvalus/production/cli/CalvalusHadoopTool.java
+++ calvalus-production/src/main/java/com/bc/calvalus/production/cli/CalvalusHadoopTool.java
@@ -1,5 +1,10 @@
 package com.bc.calvalus.production.cli;
 
+import com.bc.calvalus.processing.JobConfigNames;
+import com.bc.calvalus.processing.ProcessingService;
+import com.bc.calvalus.processing.ProcessorFactory;
+import com.bc.calvalus.processing.hadoop.HadoopProcessingService;
+import com.bc.calvalus.production.Production;
 import com.bc.calvalus.production.ProductionServiceConfig;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -13,8 +18,13 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.JobStatus;
+import org.apache.hadoop.mapred.RunningJob;
+import org.apache.hadoop.mapred.TaskCompletionEvent;
 import org.apache.hadoop.security.UserGroupInformation;
 
 import java.io.File;
@@ -23,6 +33,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.security.PrivilegedExceptionAction;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -39,6 +50,7 @@ public class CalvalusHadoopTool {
 
     private static final TypeReference<Map<String, Object>> VALUE_TYPE_REF = new TypeReference<Map<String, Object>>() {};
     private static final SimpleDateFormat ISO_MILLIS_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+    public static final String CALVALUS_SOFTWARE_PATH = "/calvalus/software/1.0";
 
     private final ObjectMapper mapper;
 
@@ -110,21 +122,35 @@ public class CalvalusHadoopTool {
         Map<String, Object> productionTypeDef = mapper.readValue(new File("etc/" + productionTypeName + "-cht-type.json"), VALUE_TYPE_REF);
 
         // create Hadoop job config with Hadoop defaults
-        String userName = System.getProperty("user.name");
-        UserGroupInformation remoteUser = UserGroupInformation.createRemoteUser(userName);
-        final Configuration jobConfTemplate = new Configuration();
-        JobClient jobClient = remoteUser.doAs((PrivilegedExceptionAction<JobClient>) () -> new JobClient(new JobConf(jobConfTemplate)));
-        JobConf jobConf = new JobConf(new Configuration(jobClient.getConf()));
+        String userName = "martin"; //System.getProperty("user.name");
+        final String productionId = Production.createId(productionTypeName);
 
-        printParameters("Hadoop defaults", jobConf);
+        final Configuration hadoopParameters = new Configuration();
+
+        printParameters("Hadoop defaults", hadoopParameters);
 
         // set parameters by tool
-        jobConf.set("calvalus.user", userName);
-        jobConf.set("mapreduce.framework.name", "yarn");
-        jobConf.set("fs.hdfs.impl.disable.cache", "true");
-        jobConf.set("mapred.mapper.new-api", "true");
-        jobConf.set("mapred.reducer.new-api", "true");
-        //jobConf.set("jobSubmissionDate", ISO_MILLIS_FORMAT.format(new Date()));
+        hadoopParameters.set("mapreduce.framework.name", "yarn");
+        hadoopParameters.set("dfs.blocksize", "2147483136");
+        hadoopParameters.set("io.file.buffer.size", "131072");
+        hadoopParameters.set("dfs.replication", "1");
+        hadoopParameters.set("dfs.client.read.shortcircuit", "true");
+        hadoopParameters.set("dfs.domain.socket.path", "/var/lib/hadoop-hdfs/dn_socket");
+        hadoopParameters.set("mapreduce.map.speculative", "false");
+        hadoopParameters.set("mapreduce.reduce.speculative", "false");
+        hadoopParameters.set("mapreduce.client.genericoptionsparser.used", "true");
+        hadoopParameters.set("dfs.permissions.superusergroup", "hadoop");
+        hadoopParameters.set("fs.permissions.umask-mode", "002");
+        hadoopParameters.set("yarn.log-aggregation-enable", "true");
+        hadoopParameters.set("yarn.app.mapreduce.am.command-opts", "-Xmx512M -Djava.awt.headless=true");
+        hadoopParameters.set("yarn.app.mapreduce.am.resource.mb", "512");
+
+        hadoopParameters.set("calvalus.user", userName);
+        hadoopParameters.set("fs.hdfs.impl.disable.cache", "true");
+        hadoopParameters.set("mapred.mapper.new-api", "true");
+        hadoopParameters.set("mapred.reducer.new-api", "true");
+        hadoopParameters.set("calvalus.output.dir", "/calvalus/home/%s/%s".format(userName, productionId));
+        //jobConfTemplate.set("jobSubmissionDate", ISO_MILLIS_FORMAT.format(new Date()));
 
         // add parameters of config, maybe translate and apply function
         for (Map.Entry<Object,Object> entry : configParameters.entrySet()) {
@@ -132,63 +158,140 @@ public class CalvalusHadoopTool {
             if (key.startsWith("calvalus.hadoop.")) {
                 key = key.substring("calvalus.hadoop.".length());
             }
-            translateAndInsert(key, String.valueOf(entry.getValue()), productionTypeDef, jobConf);
+            translateAndInsert(key, String.valueOf(entry.getValue()), productionTypeDef, hadoopParameters);
         }
 
         // add parameters of production type, maybe translate and apply function
         for (Map.Entry<String,Object> entry : productionTypeDef.entrySet()) {
             if (! entry.getKey().startsWith("_")) {
-                translateAndInsert(entry.getKey(), String.valueOf(entry.getValue()), productionTypeDef, jobConf);
+                translateAndInsert(entry.getKey(), String.valueOf(entry.getValue()), productionTypeDef, hadoopParameters);
             }
+        }
+
+        // add parameters of request, maybe translate and apply function
+        for (Map.Entry<String,Object> entry : request.entrySet()) {
+            translateAndInsert(entry.getKey(), String.valueOf(entry.getValue()), productionTypeDef, hadoopParameters);
         }
 
         // add parameters of command line, maybe translate and apply function
         for (Map.Entry<String,String> entry : commandLineParameters.entrySet()) {
-            translateAndInsert(entry.getKey(), entry.getValue(), productionTypeDef, jobConf);
+            translateAndInsert(entry.getKey(), entry.getValue(), productionTypeDef, hadoopParameters);
+        }
+
+        // create job client for user and access to file system
+        UserGroupInformation remoteUser = UserGroupInformation.createRemoteUser(userName);
+        JobClient jobClient = remoteUser.doAs((PrivilegedExceptionAction<JobClient>) () -> new JobClient(new JobConf(hadoopParameters)));
+        JobConf jobConf = new JobConf(new Configuration(jobClient.getConf()));
+        FileSystem fileSystem = jobClient.getFs();
+
+        // install processor bundles and calvalus and snap bundle
+        ProcessorFactory.installProcessorBundles(userName, jobConf, fileSystem);
+        final String calvalusBundle = jobConf.get(JobConfigNames.CALVALUS_CALVALUS_BUNDLE);
+        final String snapBundle = jobConf.get(JobConfigNames.CALVALUS_SNAP_BUNDLE);
+        if (calvalusBundle != null) {
+            installBundle(calvalusBundle, fileSystem, jobConf);
+        }
+        if (snapBundle != null) {
+            installBundle(snapBundle, fileSystem, jobConf);
         }
 
         printParameters("Parameterised job", jobConf);
 
+        RunningJob runningJob = jobClient.submitJob(jobConf);
+        System.out.println("Production successfully ordered. The production ID is: " + runningJob.getID());
 
+        Thread shutdownHook = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    runningJob.killJob();
+                } catch (Exception e) {
+                    System.err.println("Failed to shutdown production: " + e.getMessage());
+                }
+            }
+        };
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
 
-        String productionName = (String) request.get("productionName");
-        String outputDir = (String) request.get("outputDir");
-        //Job job = Job.getInstance(jobConf, productionName);
-//        FileSystem fileSystem = jobClient.getFs();
-//        ProcessorFactory.installProcessorBundles(userName, jobConf, fileSystem);
-//        final Path outputPath = new Path(outputDir);
-//        if (fileSystem.exists(outputPath)) {
-//            //fileSystem.delete(outputPath, true);
-//        }
-//        FileOutputFormat.setOutputPath(job, outputPath);
+        while (true) {
+            Thread.sleep(5000);
+            JobStatus[] allJobs = jobClient.getAllJobs();
+            JobStatus job = null;
+            for (JobStatus job1 : allJobs) {
+                if (runningJob.getID().equals(job1.getJobID())) {
+                    job = job1;
+                    break;
+                }
+            }
+            if (job == null) {
+                System.err.println("Job " + runningJob.getID() + " not found in YARN");
+                System.exit(1);
+            }
+            System.out.println("state " + job.getState() + " map " + job.getMapProgress() + " reduce " + job.getReduceProgress());
+            if (job.getRunState() == JobStatus.FAILED || job.getRunState() == JobStatus.KILLED) {
+                int eventCounter = 0;
+                while (true) {
+                    TaskCompletionEvent[] taskCompletionEvents = runningJob.getTaskCompletionEvents(eventCounter);
+                    if (taskCompletionEvents.length == 0) {
+                        break;
+                    }
+                    eventCounter += taskCompletionEvents.length;
+                    for (TaskCompletionEvent taskCompletionEvent : taskCompletionEvents) {
+                        if (taskCompletionEvent.getTaskStatus().equals(TaskCompletionEvent.Status.FAILED)) {
+                            String[] taskDiagnostics = runningJob.getTaskDiagnostics(taskCompletionEvent.getTaskAttemptId());
+                            if (taskDiagnostics.length > 0) {
+                                String firstMessage = taskDiagnostics[0];
+                                String firstLine = firstMessage.split("\\n")[0];
+                                String[] firstLineSplit = firstLine.split("Exception: ");
+                                System.err.println(firstLineSplit[firstLineSplit.length - 1]);
+                                break;
+                            }
+                        }
+                    }
+                }
+                System.exit(1);
+            } else if (job.getRunState() == JobStatus.SUCCEEDED) {
+               break;
+            }
+        }
+        Runtime.getRuntime().removeShutdownHook(shutdownHook);
 
-        //RunningJob runningJob = jobClient.submitJob(jobConf);
+        //if (production.getProcessingStatus().getState() == ProcessState.COMPLETED) {
+        //    say("Production completed. Output directory is " + production.getStagingPath());
+        //} else {
+        //    exit("Error: Production did not complete normally: " + production.getProcessingStatus().getMessage(), 2);
+        //}
     }
 
-    private void translateAndInsert(String key, String value, Map<String, Object> productionTypeDef, JobConf jobConf)
+    private void installBundle(String calvalusBundle, FileSystem fileSystem, JobConf jobConf) throws IOException {
+        Path bundlePath = new Path(CALVALUS_SOFTWARE_PATH, calvalusBundle);
+        HadoopProcessingService.addBundleToClassPathStatic(bundlePath, jobConf, fileSystem);
+        HadoopProcessingService.addBundleArchives(bundlePath, fileSystem, jobConf);
+        HadoopProcessingService.addBundleLibs(bundlePath, fileSystem, jobConf);
+    }
+
+    private void translateAndInsert(String key, String value, Map<String, Object> productionTypeDef, Configuration jobConf)
             throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
         String translationKey = "_translate." + key;
-        if (! productionTypeDef.containsKey(translationKey)) {
+        Object translations = productionTypeDef.get(translationKey);
+        if (translations == null) {
             jobConf.set(key, value);
+        } else if (translations instanceof String) {
+            jobConf.set((String) translations, value);
         } else {
-            for (String translation : ((String) productionTypeDef.get(translationKey)).split(";")) {
-                String hadoopKey;
-                String hadoopValue;
-                if (! translation.contains(",")) {
-                    hadoopKey = translation.trim();
-                    hadoopValue = value;
+            for (Object translation : (ArrayList<Object>) translations) {
+                if (translation instanceof String) {
+                    jobConf.set((String) translation, value);
                 } else {
-                    String[] translationElements = translation.split(",");
-                    hadoopKey = translationElements[0].trim();
-                    String functionName = translationElements[1].trim();
-                    hadoopValue = (String) getClass().getMethod(functionName, String.class).invoke(this, value);
+                    String hadoopKey = String.valueOf(((ArrayList<Object>) translation).get(0));
+                    String functionName = String.valueOf(((ArrayList<Object>) translation).get(1));
+                    String hadoopValue = (String) getClass().getMethod(functionName, String.class).invoke(this, value);
+                    jobConf.set(hadoopKey, hadoopValue);
                 }
-                jobConf.set(hadoopKey, hadoopValue);
             }
         }
     }
 
-    private void printParameters(String header, JobConf jobConf) {
+    private void printParameters(String header, Configuration jobConf) {
         System.out.println(header);
         System.out.println(jobConf);
         Iterator<Map.Entry<String, String>> iterator = jobConf.iterator();
@@ -198,7 +301,6 @@ public class CalvalusHadoopTool {
         }
         System.out.println();
     }
-
 
     private static String getParameter(Map<String, Object> request, String... names) {
         for (String name : names) {
