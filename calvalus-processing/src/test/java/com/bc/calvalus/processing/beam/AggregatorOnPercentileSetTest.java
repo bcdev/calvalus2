ca16026bb298976aeac443db81e58d5ff2cf156f
TomBlock <tom.block@brockmann-consult.de>
2019-10-14 16:13:42 +0200

    update to latest SNAP changes, changed jetspeed plugin version, was incompatible with maven 3.6

--- calvalus-processing/src/test/java/com/bc/calvalus/processing/beam/AggregatorOnPercentileSetTest.java
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/beam/AggregatorOnPercentileSetTest.java
@@ -156,25 +156,8 @@ public class AggregatorOnPercentileSetTest {
         return new ObservationImpl(0.0, 0.0, mjd, values);
     }
 
-    public static BinContext createCtx() {
-        return new BinContext() {
-            private HashMap<String, Object> map = new HashMap<String, Object>();
-
-            @Override
-            public long getIndex() {
-                return 0;
-            }
-
-            @Override
-            public <T> T get(String name) {
-                return (T) map.get(name);
-            }
-
-            @Override
-            public void put(String name, Object value) {
-                map.put(name, value);
-            }
-        };
+    private static BinContext createCtx() {
+        return new TestBinContext();
     }
 
     static class MyVariableContext implements VariableContext {
---
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/beam/TestBinContext.java
@@ -0,0 +1,30 @@
+package com.bc.calvalus.processing.beam;
+
+import org.esa.snap.binning.BinContext;
+
+import java.util.HashMap;
+
+public class TestBinContext implements BinContext {
+
+    private HashMap<String, Object> map = new HashMap<String, Object>();
+
+    @Override
+    public long getIndex() {
+        return 0;
+    }
+
+    @Override
+    public <T> T get(String name) {
+        return (T) map.get(name);
+    }
+
+    @Override
+    public void put(String name, Object value) {
+        map.put(name, value);
+    }
+
+    @Override
+    public String ensureUnique(String name) {
+        throw new RuntimeException("not imlemented");
+    }
+}
