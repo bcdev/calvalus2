c6d6da06ba30fa91959415144f84f716e31d3a50
TomBlock <tom.block@brockmann-consult.de>
2019-10-14 16:33:45 +0200

    just testing

---
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/IntegrationTestPartFiles.java
@@ -0,0 +1,12 @@
+package com.bc.calvalus.processing;
+
+import org.apache.hadoop.fs.LocalFileSystem;
+import org.junit.Test;
+
+public class IntegrationTestPartFiles {
+
+    @Test
+    public void testReadSEA_part()  {
+        final LocalFileSystem localFileSystem = new LocalFileSystem();
+    }
+}
