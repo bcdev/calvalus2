17723578ca538d075efd947c97e2f52ec0432b0f
martin-boettcher <martin.boettcher@brockmann-consult.de>
2020-08-13 19:36:05 +0200

    make sure auxdata is available before engine is started

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/ProcessorAdapter.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/ProcessorAdapter.java
@@ -119,6 +119,12 @@ public abstract class ProcessorAdapter {
                 System.setProperty("snap.gpf.allowAuxdataDownload", "false");
             }
         }
+        // we need the auxdata tree before the engine is started
+        try {
+            shallowCopyPatches(new File(".").getAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException("processor adapter initialisation failed", e);
+        }
         GpfUtils.init(conf);
         Engine.start();
         CalvalusLogger.restoreCalvalusLogFormatter();
@@ -138,10 +144,11 @@ public abstract class ProcessorAdapter {
 
     /**
      * Prepares the processing.
-     * The default implementation does nothing except creating a shallow copy tree with symlinks to files of patch packages.
+     * The default implementation does nothing.
+     * The creation of a shallow copy tree with symlinks to files of patch packages has been moved to before engine start.
      */
     public void prepareProcessing() throws IOException {
-        shallowCopyPatches(new File(".").getAbsolutePath());
+        //shallowCopyPatches(new File(".").getAbsolutePath());
     }
 
     protected static void shallowCopyPatches(String wd) throws IOException {
