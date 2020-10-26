cbb7516a66ec6e84a8a3690c5ddde1498f864f1a
martin-boettcher <martin.boettcher@brockmann-consult.de>
2020-10-26 17:48:05 +0100

    added first test for CRS geocoding for L3

--- calvalus-processing/src/test/java/com/bc/calvalus/processing/l3/L3MapperTest.java
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/l3/L3MapperTest.java
@@ -30,16 +30,24 @@ import org.esa.snap.binning.PlanetaryGrid;
 import org.esa.snap.binning.VariableContext;
 import org.esa.snap.binning.operator.BinningConfig;
 import org.esa.snap.binning.support.BinningContextImpl;
+import org.esa.snap.binning.support.CrsGrid;
 import org.esa.snap.core.dataio.ProductIO;
+import org.esa.snap.core.datamodel.CrsGeoCoding;
+import org.esa.snap.core.datamodel.GeoPos;
 import org.esa.snap.core.datamodel.MetadataAttribute;
 import org.esa.snap.core.datamodel.MetadataElement;
+import org.esa.snap.core.datamodel.PixelPos;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
 import org.esa.snap.core.datamodel.VirtualBand;
 import org.esa.snap.core.gpf.GPF;
-import org.esa.snap.core.util.StringUtils;
+import org.esa.snap.core.util.SystemUtils;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.operation.projection.MapProjection;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
 
 import java.io.File;
 import java.io.IOException;
@@ -162,6 +170,43 @@ public class L3MapperTest {
         subsetParams.put("region", conf.get("calvalus.region"));
         Product subset2 = GPF.createProduct("Subset", subsetParams, product);
         ProductIO.writeProduct(subset2, conf.get("calvalus.output.dir") + "/testpixelsubset.dim", "BEAM-DIMAP");
+    }
+
+    @Test
+    public void testCrsGridConversion() throws FactoryException, TransformException {
+        SystemUtils.initGeoTools();
+        MapProjection.class.getClassLoader().setClassAssertionStatus(MapProjection.class.getName(), false);
+        CrsGeoCoding inputGeoCoding = new CrsGeoCoding(CRS.decode("EPSG:32636"),
+                                                     1830,
+                                                     1830,
+                                                     166020,
+                                                     5100000,
+                                                     60,
+                                                     -60,
+                                                     0,
+                                                     0);
+        System.out.println(inputGeoCoding);
+        double epsDegFor1m = 180.0 / 20000000;
+        assertEquals("eps", epsDegFor1m, 0.000009, epsDegFor1m);
+        GeoPos geoPos = new GeoPos();
+        inputGeoCoding.getGeoPos(new PixelPos(0/60+0.5, 0/60+0.5), geoPos);
+        assertEquals("lat", 45.972471023394014, geoPos.lat, epsDegFor1m);
+        assertEquals("lon", 28.689203148615423, geoPos.lon, epsDegFor1m);
+
+        CrsGrid grid = new CrsGrid(60.0, "EPSG:32636");
+        long binIndex = grid.getBinIndex(geoPos.lat, geoPos.lon);
+        assertEquals("bin", 784687239, binIndex);
+
+        double[] centerLatLon = grid.getCenterLatLon(binIndex);
+        assertEquals("lat", geoPos.lat, centerLatLon[0], epsDegFor1m);
+        assertEquals("lon", geoPos.lon, centerLatLon[1], epsDegFor1m);
+
+        long binIndex2 = grid.getBinIndex(centerLatLon[0], centerLatLon[1]);
+        assertEquals("bin", binIndex, binIndex2);
+
+        double[] centerLatLon2 = grid.getCenterLatLon(binIndex2);
+        assertEquals("lat", centerLatLon[0], centerLatLon2[0], epsDegFor1m);
+        assertEquals("lon", centerLatLon[1], centerLatLon2[1], epsDegFor1m);
     }
 
 }
