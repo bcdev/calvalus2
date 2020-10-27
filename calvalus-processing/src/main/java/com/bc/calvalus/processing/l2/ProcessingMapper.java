c42e00c0b54cd174872ad79c21a4561365b4cf0e
martin-boettcher <martin.boettcher@brockmann-consult.de>
2020-10-27 15:12:56 +0100

    align formatting parameters of ProcessingMapper with L3Reducer

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/l2/ProcessingMapper.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/l2/ProcessingMapper.java
@@ -212,7 +212,7 @@ public class ProcessingMapper extends Mapper<NullWritable, NullWritable, Text /*
             // check and prepare, localise
 
             long t0 = System.currentTimeMillis();
-            if (productFormatter != null && checkFormattedOutputExists(jobConfig, context, productFormatter.getOutputFilename())) { return; }
+            if (productFormatter != null && !jobConfig.getBoolean(JobConfigNames.FORCE_REPROCESS, false) && checkFormattedOutputExists(jobConfig, context, productFormatter.getOutputFilename())) { return; }
             processorAdapter.prepareProcessing();
             if (checkNativeOutputExists(jobConfig, context, processorAdapter)) { return; }
             if (checkInputIntersectsRoi(jobConfig, context, processorAdapter)) { return; }
@@ -308,8 +308,8 @@ public class ProcessingMapper extends Mapper<NullWritable, NullWritable, Text /*
 
 
     protected String getProductName(Configuration jobConfig, String fileName) {
-        String regex = jobConfig.get(JobConfigNames.OUTPUT_REGEX, "^(.*)$");
-        String replacement = jobConfig.get(JobConfigNames.OUTPUT_REPLACEMENT, "L2_of_$1");
+        String regex = jobConfig.get(JobConfigNames.CALVALUS_OUTPUT_REGEX, "^(.*)$");
+        String replacement = jobConfig.get(JobConfigNames.CALVALUS_OUTPUT_REPLACEMENT, "L2_of_$1");
         String dateElement = jobConfig.get(JobConfigNames.OUTPUT_DATE_ELEMENT, null);
         String dateFormat = jobConfig.get(JobConfigNames.OUTPUT_DATE_FORMAT, null);
         try {
@@ -630,7 +630,7 @@ public class ProcessingMapper extends Mapper<NullWritable, NullWritable, Text /*
         LOG.info("Start writing product to file: " + productFile.getName());
 
         //ProductIO.writeProduct(targetProduct, productFile, outputFormat, false, pm);
-        Boolean writeEntireTileRows = Boolean.valueOf(System.getProperty("snap.dataio.writer.writeEntireTileRows", "true"));
+        Boolean writeEntireTileRows = Boolean.valueOf(jobConfig.get("snap.dataio.writer.writeEntireTileRows", "true"));
         GPF.writeProduct(targetProduct, productFile, outputFormat, writeEntireTileRows, false, false, pm);
         LOG.info("formatting done in [ms]: " + (System.currentTimeMillis() - t0));
 
