03b22f685d7008cd3a47a9c5482891b6e4021b4f
martin-boettcher <martin.boettcher@brockmann-consult.de>
2021-02-03 21:01:51 +0100

    remove unused imports of deprecated classes

--- calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/S3aCompatibleOutputFormat.java
+++ calvalus-processing/src/main/java/com/bc/calvalus/processing/beam/S3aCompatibleOutputFormat.java
@@ -17,18 +17,15 @@
 package com.bc.calvalus.processing.beam;
 
 import com.bc.calvalus.commons.CalvalusLogger;
-import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.InvalidJobConfException;
 import org.apache.hadoop.mapreduce.JobContext;
-import org.apache.hadoop.mapreduce.JobStatus;
 import org.apache.hadoop.mapreduce.OutputCommitter;
 import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputCommitter;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.IOException;
 import java.util.logging.Logger;
