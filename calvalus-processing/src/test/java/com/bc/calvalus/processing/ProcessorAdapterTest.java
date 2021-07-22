da76d1ff4170909440a7f886dfce21bcb111456f
martin-boettcher <martin.boettcher@brockmann-consult.de>
2021-07-22 18:02:47 +0200

    test for one-time analysis of geo-coding issue added

--- calvalus-processing/src/test/java/com/bc/calvalus/processing/ProcessorAdapterTest.java
+++ calvalus-processing/src/test/java/com/bc/calvalus/processing/ProcessorAdapterTest.java
@@ -16,12 +16,23 @@
 
 package com.bc.calvalus.processing;
 
-import com.bc.calvalus.processing.beam.SnapOperatorAdapter;
+import com.bc.calvalus.processing.executable.ExecutableProcessorAdapter;
+import com.bc.calvalus.processing.hadoop.ProductSplit;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.MapTask;
+import org.apache.hadoop.mapreduce.MapContext;
+import org.apache.hadoop.mapreduce.StatusReporter;
+import org.apache.hadoop.mapreduce.task.MapContextImpl;
+import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.awt.Rectangle;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class ProcessorAdapterTest {
 
@@ -42,6 +53,24 @@ public class ProcessorAdapterTest {
     @Test
     public void testShallowCopyPatches() throws Exception {
         ProcessorAdapter.shallowCopyPatches("/home/boe/tmp/caltest");
+    }
+
+    @Ignore
+    @Test
+    public void testGetInputRectangle() throws IOException {
+        Configuration conf = new Configuration();
+        ProductSplit productSplit = new ProductSplit(new Path("/windows/tmp/MSIL1C-60m-7blk-36T-A-107-20210718-v01.2.nc"), 1887624736L, new String[] { "localhost" }, -1, -1);
+        conf.set(JobConfigNames.CALVALUS_REGION_GEOMETRY, "POLYGON((30 40,36 40,36 48,30 48,30 40))");
+        MapTask mapTask = new MapTask();
+        StatusReporter reporter = new TaskAttemptContextImpl.DummyReporter();
+        MapContext mapContext = new MapContextImpl(conf, mapTask.getTaskID(), null, null, null, reporter, productSplit);
+        ExecutableProcessorAdapter processorAdapter = new ExecutableProcessorAdapter(mapContext);
+        // cuts off northern part of product while getting border pixels
+        // set breakpoint at GeoUtils:684         regionIntersection.apply(pixelRegionFinder);
+        // set breakpoint at GeoUtils:778:        final PixelPos pixelPos = geoCoding.getPixelPos(geoPos, null);
+        // getPixelPos fails for the northern border
+        Rectangle inputRectangle = processorAdapter.getInputRectangle();
+        System.out.println(inputRectangle);
     }
 
 }
